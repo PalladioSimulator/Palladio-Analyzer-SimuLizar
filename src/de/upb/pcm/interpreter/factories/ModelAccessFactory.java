6b40b6ac8876bb3c99daec5290f8cc728afadc34
thomas_zolynski <unknown.mail@emailaddr>
2011-12-13 15:18:09 +0000

    Sources and JavaDoc for SimuLizar v1.0 by Joachim Meyer.
    See also http://sdqweb.ipd.kit.edu/wiki/SimuLizar
    
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk/de.upb.pcm.interpreter@13871 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ src/de/upb/pcm/interpreter/factories/ModelAccessFactory.java
@@ -0,0 +1,125 @@
+package de.upb.pcm.interpreter.factories;
+
+
+import de.uka.ipd.sdq.pcm.core.composition.AssemblyContext;
+import de.upb.pcm.interpreter.access.AbstractPCMModelAccess;
+import de.upb.pcm.interpreter.access.AbstractPMSModelAccess;
+import de.upb.pcm.interpreter.access.AllocationAccess;
+import de.upb.pcm.interpreter.access.PMSAccess;
+import de.upb.pcm.interpreter.access.ResourceEnvironmentAccess;
+import de.upb.pcm.interpreter.access.SystemAccess;
+import de.upb.pcm.interpreter.access.UsageModelAccess;
+import de.upb.pcm.interpreter.interfaces.IModelAccessFactory;
+import de.upb.pcm.interpreter.interpreter.AbstractPCMModelInterpreter;
+import de.upb.pcm.interpreter.interpreter.RDSeffInterpreter;
+import de.upb.pcm.interpreter.interpreter.RepositoryInterpreter;
+import de.upb.pcm.interpreter.interpreter.UsageModelUsageScenarioInterpreter;
+import de.upb.pcm.interpreter.simulation.InterpreterDefaultContext;
+import de.upb.pcm.interpreter.utils.ModelHelper;
+
+
+/**
+ * Factory for pcm and pms model accesses and pcm model interpreters.
+ * 
+ * @author Joachim Meyer
+ * 
+ */
+public class ModelAccessFactory implements IModelAccessFactory
+{
+
+
+   private final ModelHelper modelHelper;
+
+
+   /**
+    * Constructor
+    * 
+    * @param modelHelper the model helper.
+    */
+   public ModelAccessFactory(final ModelHelper modelHelper)
+   {
+      super();
+      this.modelHelper = modelHelper;
+
+
+   }
+
+
+   /**
+    * @return the modelHelper.
+    */
+   private ModelHelper getModelHelper()
+   {
+      return this.modelHelper;
+   }
+
+
+   /**
+    * 
+    * @see de.upb.pcm.interpreter.interfaces.IModelAccessFactory#getPCMModelAccess(int,
+    *      de.upb.pcm.interpreter.simulation.InterpreterDefaultContext)
+    */
+   @Override
+   public AbstractPCMModelAccess getPCMModelAccess(final int modelAccessType, final InterpreterDefaultContext context)
+   {
+
+      switch (modelAccessType)
+      {
+         case SYSTEM_ACCESS:
+            return new SystemAccess(context, getModelHelper());
+         case RESOURCE_ENVIRONMENT_ACCESS:
+            return new ResourceEnvironmentAccess(context, getModelHelper());
+         case ALLOCATION_ACCESS:
+            return new AllocationAccess(context, getModelHelper());
+         case USAGE_MODEL_ACCESS:
+            return new UsageModelAccess(context, getModelHelper());
+
+         default:
+            throw new IllegalArgumentException("No reader for readerType " + modelAccessType + " found");
+
+      }
+   }
+
+
+   /**
+    * 
+    * @see de.upb.pcm.interpreter.interfaces.IModelAccessFactory#getPCMModelInterpreter(int,
+    *      de.upb.pcm.interpreter.simulation.InterpreterDefaultContext,
+    *      de.uka.ipd.sdq.pcm.core.composition.AssemblyContext)
+    */
+   @Override
+   public AbstractPCMModelInterpreter getPCMModelInterpreter(final int interpreterType,
+         final InterpreterDefaultContext context, final AssemblyContext assemblyContext)
+   {
+
+      switch (interpreterType)
+      {
+
+         case USAGEMODEL_USAGESCENARIO_INTERPRETER:
+            return new UsageModelUsageScenarioInterpreter(context, getModelHelper());
+
+         case RDSEFF_INTERPRETER:
+            return new RDSeffInterpreter(context, getModelHelper(), assemblyContext);
+
+         case REPOSITORY_INTERPRETER:
+            return new RepositoryInterpreter(context, getModelHelper());
+
+         default:
+            throw new IllegalArgumentException("No interpreter for interpreterType " + interpreterType + " found");
+
+      }
+   }
+
+
+   /**
+    * 
+    * @see de.upb.pcm.interpreter.interfaces.IModelAccessFactory#getPMSModelAccess()
+    */
+   @Override
+   public AbstractPMSModelAccess getPMSModelAccess()
+   {
+      return new PMSAccess(getModelHelper());
+   }
+
+
+}
---
+++ src/de/upb/pcm/interpreter/utils/PCMInterpreterProbeSpecUtil.java
@@ -0,0 +1,132 @@
+package de.upb.pcm.interpreter.utils;
+
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EObject;
+
+import de.uka.ipd.sdq.probespec.framework.ISampleBlackboard;
+import de.uka.ipd.sdq.probespec.framework.ProbeSample;
+import de.uka.ipd.sdq.probespec.framework.ProbeSetSample;
+import de.uka.ipd.sdq.probespec.framework.ProbeSpecContext;
+import de.uka.ipd.sdq.probespec.framework.ProbeType;
+import de.uka.ipd.sdq.probespec.framework.calculator.Calculator;
+import de.uka.ipd.sdq.probespec.framework.calculator.CalculatorRegistry;
+import de.uka.ipd.sdq.probespec.framework.calculator.ICalculatorFactory;
+import de.uka.ipd.sdq.probespec.framework.calculator.ResponseTimeCalculator;
+import de.uka.ipd.sdq.probespec.framework.probes.IProbeStrategy;
+import de.uka.ipd.sdq.probespec.framework.utils.ProbeSpecUtils;
+import de.uka.ipd.sdq.simucomframework.abstractSimEngine.SimProcess;
+import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
+import de.upb.pcm.pms.MeasurementSpecification;
+
+
+/**
+ * Util for the Probe Specification Framework.
+ * 
+ * @author Joachim Meyer
+ * 
+ */
+public class PCMInterpreterProbeSpecUtil
+{
+
+   protected static final Logger logger = Logger.getLogger(PCMInterpreterProbeSpecUtil.class.getName());
+
+   final ISampleBlackboard blackboard = ProbeSpecContext.instance().getSampleBlackboard();
+
+   private final ModelHelper modelHelper;
+
+
+   /**
+    * Constructor
+    * 
+    * @param modelHelper the model helper.
+    */
+   public PCMInterpreterProbeSpecUtil(final ModelHelper modelHelper)
+   {
+      super();
+      this.modelHelper = modelHelper;
+
+   }
+
+
+   /**
+    * Creates a response time calculator in the probe specification framework.
+    * 
+    * @param startProbeId id of the start probe.
+    * @param stopProbeId id of the stop probe.
+    * @param measurementId id of this measurement.
+    * @param calculatorName name of the response time calculator
+    * @param measurementSpecification the corresponding measurement specification from the pms
+    *           model.
+    * @param pcmElement the pcm element to be measured.
+    * @param simuComModel the SimuCom model.
+    * @return the created calculator, null if calculator already exists.
+    */
+   public Calculator createResponseTimeCalculator(final String startProbeId, final String stopProbeId,
+         final String measurementId, final String calculatorName,
+         final MeasurementSpecification measurementSpecification, final EObject pcmElement,
+         final SimuComModel simuComModel)
+   {
+
+      ResponseTimeCalculator responseTimeCalculator = null;
+      final ProbeSpecContext probeSpecContext = ProbeSpecContext.instance();
+      final CalculatorRegistry calculatorRegistry = probeSpecContext.getCalculatorRegistry();
+      // only register each calculator once
+      if (calculatorRegistry.getCalculatorForId(measurementId) == null)
+      {
+         final ICalculatorFactory calculatorFactory = probeSpecContext.getCalculatorFactory();
+
+         final Calculator responseCalculator = calculatorFactory.buildResponseTimeCalculator(calculatorName,
+               probeSpecContext.obtainProbeSetId(startProbeId), probeSpecContext.obtainProbeSetId(stopProbeId));
+
+         calculatorRegistry.registerCalculator(measurementId, responseCalculator);
+
+         InterpreterLogger.debug(logger, "Created Response Time Calculator. StartProbeId: " + startProbeId
+               + ", StopProbeId: " + stopProbeId);
+
+         // get created calculator
+         responseTimeCalculator = (ResponseTimeCalculator) calculatorRegistry.getCalculatorForId(measurementId);
+
+         return responseTimeCalculator;
+
+      }
+      return null;
+
+   }
+
+
+   /**
+    * @return returns the blackboard.
+    */
+   private ISampleBlackboard getBlackboard()
+   {
+      return this.blackboard;
+   }
+
+
+   /**
+    * Takes current time sample.
+    * 
+    * @param probeID the start or stop probe id.
+    * @param measurementId id of the measurement.
+    * @param simProcess the sim process.
+    */
+   @SuppressWarnings({ "rawtypes", "unchecked" })
+   public void takeCurrentTimeSample(final String probeID, final String measurementId, final SimProcess simProcess)
+   {
+      final IProbeStrategy timeStrategy = ProbeSpecContext.instance().getProbeStrategyRegistry()
+            .getProbeStrategy(ProbeType.CURRENT_TIME, null);
+
+      final ProbeSample probeSample = timeStrategy.takeSample(probeID, this.modelHelper.getSimuComModel()
+            .getSimulationControl());
+
+      final ProbeSetSample probeSampleSet = ProbeSpecUtils.buildProbeSetSample(probeSample,
+            simProcess.getRequestContext(), measurementId, ProbeSpecContext.instance().obtainProbeSetId(probeID));
+
+      InterpreterLogger.debug(logger, "Took probe " + probeID + " of " + measurementId);
+
+      getBlackboard().addSample(probeSampleSet);
+   }
+
+
+}
