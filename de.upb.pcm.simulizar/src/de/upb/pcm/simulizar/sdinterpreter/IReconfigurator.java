f6924028e426319595b42874e29346cef24986f8
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2012-05-10 04:52:47 +0000

    More doc
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@15213 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.upb.pcm.simulizar/src/de/upb/pcm/simulizar/sdinterpreter/IReconfigurator.java
+++ de.upb.pcm.simulizar/src/de/upb/pcm/simulizar/sdinterpreter/IReconfigurator.java
@@ -3,13 +3,19 @@ package de.upb.pcm.simulizar.sdinterpreter;
 import org.eclipse.emf.ecore.EObject;
 
 /**
- * Interface for a component that is able to reconfigure the model@runtime.
+ * Interface for a component that is able to reconfigure the PCM model@runtime.
  * 
  * @author snowball
  * 
  */
 public interface IReconfigurator {
 
+    /**
+     * Trigger a reconfiguration of the model@runtime. Clients should both check whether they
+     * should reconfigure and do the reconfiguration if the check is positive.
+     * 
+     * @param monitoredElement PCM model element for which a new sensor measurement arrived. 
+     */
     public abstract void runReconfiguration(EObject monitoredElement);
 
 }
