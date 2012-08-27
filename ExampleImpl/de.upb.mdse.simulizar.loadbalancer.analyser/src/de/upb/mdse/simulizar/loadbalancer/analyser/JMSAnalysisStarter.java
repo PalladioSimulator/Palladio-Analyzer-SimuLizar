90cdd8239209ed3c56d5c9c8518ca4962539e4d0
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2012-08-27 15:17:41 +0000

    Initial import of skeleton analyser
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@16158 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ ExampleImpl/de.upb.mdse.simulizar.loadbalancer.analyser/src/de/upb/mdse/simulizar/loadbalancer/analyser/JMSAnalysisStarter.java
@@ -0,0 +1,64 @@
+package de.upb.mdse.simulizar.loadbalancer.analyser;
+
+import kieker.analysis.AnalysisController;
+import kieker.analysis.exception.AnalysisConfigurationException;
+import kieker.analysis.plugin.filter.forward.TeeFilter;
+import kieker.analysis.plugin.reader.jms.JMSReader;
+import kieker.common.configuration.Configuration;
+
+public final class JMSAnalysisStarter {
+
+	private static final String CONNECTION_FACTORY_TYPE__HORNETQ = "org.jnp.interfaces.NamingContextFactory";
+
+	/** Default provider URL used by HornetQ */
+	private static final String PROVIDER_URL__HORNETQ = "jnp://localhost:1100/";
+
+	private static final String QUEUE_HORNETQ = "monitoring";
+
+	private static String connectionFactory;
+	private static String providerUrl;
+	private static String queue;
+
+	private JMSAnalysisStarter() {}
+
+	public static void main(final String[] args) {
+		JMSAnalysisStarter.configure();
+		
+		final AnalysisController analysisInstance = new AnalysisController();
+
+		final Configuration logReaderConfiguration = new Configuration();
+		logReaderConfiguration.setProperty(JMSReader.CONFIG_PROPERTY_NAME_PROVIDERURL, providerUrl);
+		logReaderConfiguration.setProperty(JMSReader.CONFIG_PROPERTY_NAME_FACTORYLOOKUP, connectionFactory);
+		logReaderConfiguration.setProperty(JMSReader.CONFIG_PROPERTY_NAME_DESTINATION, queue);
+
+		final JMSReader logReader = new JMSReader(logReaderConfiguration);
+		analysisInstance.registerReader(logReader);
+
+		/* Create and register a simple output writer. */
+		final TeeFilter teeFilter = new TeeFilter(new Configuration());
+		analysisInstance.registerFilter(teeFilter);
+
+		try {
+			analysisInstance.connect(logReader, JMSReader.OUTPUT_PORT_NAME_RECORDS, teeFilter, TeeFilter.INPUT_PORT_NAME_EVENTS);
+			analysisInstance.run();
+		} catch (final AnalysisConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Parses the given arguments and initializes the variables {@link #connectionFactory} and {@link #providerUrl}.
+	 * 
+	 * @return true iff valid parameters were passed
+	 */
+	private static boolean configure() {
+		connectionFactory = CONNECTION_FACTORY_TYPE__HORNETQ;
+		System.out.println("jms-connection-factory:" + connectionFactory);
+		providerUrl = PROVIDER_URL__HORNETQ;
+		System.out.println("jms-provider-url:      " + providerUrl);
+		queue = QUEUE_HORNETQ;
+		System.out.println("jms-queue:             " + queue);
+		System.out.println();
+		return true;
+	}
+}
