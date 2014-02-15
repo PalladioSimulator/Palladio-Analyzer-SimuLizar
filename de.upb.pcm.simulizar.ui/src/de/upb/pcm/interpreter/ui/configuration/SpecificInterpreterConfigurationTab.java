544285b5532a02d8b74178e839531fb0aab14709
suman_jojiju <unknown.mail@emailaddr>
2014-02-15 11:45:53 +0000

    simulizar specific file configuration tab
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@20764 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.upb.pcm.simulizar.ui/src/de/upb/pcm/interpreter/ui/configuration/SpecificInterpreterConfigurationTab.java
@@ -0,0 +1,17 @@
+package de.upb.pcm.interpreter.ui.configuration;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuTabGroup;
+
+
+public class SpecificInterpreterConfigurationTab  extends SimuTabGroup{
+	public void createTabs(final ILaunchConfigurationDialog dialog,
+			final String mode) {
+		 super.createTabs(dialog, mode);
+		 // change file configuration tab
+		 super.getTabs()[0] = new SimuInterpreterFileNamesInputTab();
+		
+   }
+}
+
+
+
