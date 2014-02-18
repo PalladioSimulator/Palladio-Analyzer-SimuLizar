d50ff97ff7b9fbf24e2256caa9120022425b7fc7
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2014-02-18 10:03:47 +0000

    Refactored and renamed PMS
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@20797 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.upb.pcm.pms/src/org/palladiosimulator/simulizar/pms/Intervall.java
@@ -0,0 +1,59 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.palladiosimulator.simulizar.pms;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Intervall</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <!-- begin-model-doc -->
+ * Simple intervall.
+ * <!-- end-model-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.palladiosimulator.simulizar.pms.Intervall#getIntervall <em>Intervall</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.palladiosimulator.simulizar.pms.PmsPackage#getIntervall()
+ * @model
+ * @generated
+ */
+public interface Intervall extends TemporalCharacterization
+{
+   /**
+     * Returns the value of the '<em><b>Intervall</b></em>' attribute.
+     * The default value is <code>"0.0"</code>.
+     * <!-- begin-user-doc -->
+    * <p>
+    * If the meaning of the '<em>Intervall</em>' attribute isn't clear,
+    * there really should be more of a description here...
+    * </p>
+    * <!-- end-user-doc -->
+     * @return the value of the '<em>Intervall</em>' attribute.
+     * @see #setIntervall(double)
+     * @see org.palladiosimulator.simulizar.pms.PmsPackage#getIntervall_Intervall()
+     * @model default="0.0" required="true"
+     * @generated
+     */
+   double getIntervall();
+
+   /**
+     * Sets the value of the '{@link org.palladiosimulator.simulizar.pms.Intervall#getIntervall <em>Intervall</em>}' attribute.
+     * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+     * @param value the new value of the '<em>Intervall</em>' attribute.
+     * @see #getIntervall()
+     * @generated
+     */
+   void setIntervall(double value);
+
+} // Intervall
