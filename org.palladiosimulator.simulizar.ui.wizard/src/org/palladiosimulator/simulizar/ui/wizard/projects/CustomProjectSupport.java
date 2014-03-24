7e3393e9ace41d74d43d9d43c193e298db544ffc
igor_rogic <unknown.mail@emailaddr>
2014-03-24 09:20:13 +0000

    Changed the structure of files to reflect the original.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@21374 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.ui.wizard/src/org/palladiosimulator/simulizar/ui/wizard/projects/CustomProjectSupport.java
+++ org.palladiosimulator.simulizar.ui.wizard/src/org/palladiosimulator/simulizar/ui/wizard/projects/CustomProjectSupport.java
@@ -35,47 +35,55 @@ public class CustomProjectSupport {
 		IProject project = createBaseProject(projectName, location);
 		try {
 			addNature(project);
-			String[] paths = {"data", ".settings"}; //$NON-NLS-1$ //$NON-NLS-2$
-			String[] files0 = {
-					"acquireResources.sdm",
-					"acquireResources.sdm_diagram",
-					"EvaluationSpecification.pms",
+			// list of project directories
+			String[] paths = {
+					".settings", "monitors", "reconf_qvto", "results", "rules_active", "rules_sd" }; //$NON-NLS-1$ //$NON-NLS-2$
+			// creating directories
+			addToProjectStructure(project, paths);
+
+			// adding files to the root directory
+			String[] filesArray = {
 					"lastverteiler.entpacke.seff_diagram",
-					"releaseResources.sdm",
-					"releaseResources.sdm_diagram",
-					"resourceProcessingRateReconfigurationStrategy.storydiagrams_diagram",
-					"resourceProcessingRateReconfigurationStrategy.storydiagrams1",
-					"server.entpacke.seff_diagram", "Simulizar Example.launch",
-					"testSystem.allocation", "testSystem.allocation_diagram",
-					"testSystem.repository", "testSystem.repository_diagram",
+					"server.entpacke.seff_diagram",
+					"Simulizar Example.launch",
+					"testSystem.allocation",
+					"testSystem.allocation_diagram",
+					"testSystem.repository",
+					"testSystem.repository_diagram",
 					"testSystem.resourceenvironment",
 					"testSystem.resourceenvironment_diagram",
-					"testSystem.system", "testSystem.system_diagram",
-					"testSystem.usagemodel", "testSystem.usagemodel_diagram" };
-			List<String> files2 = new ArrayList<String>();
-			files2.add("experiment.ser");
-			files2.add("exprun.ser");
-			files2.add("id_generator.ser");
-			files2.add("sensor.ser");
-			files2.add("state.ser");
-			for(int i = 0; i <= 3; i++){
-				for(int j = 0; j <= 15; j++){
-					files2.add("exprun" + i + "_" + j + "_ET.ser");
-					files2.add("exprun" + i + "_" + j + "_MEAS.ser");
-				}
-			}
-			for(int i = 5; i <= 46; i++){
-				for(int j = 16; j <= 31; j++){
-					files2.add("exprun" + i + "_" + j + "_ET.ser");
-					files2.add("exprun" + i + "_" + j + "_MEAS.ser");
-				}
-			}
-			List<String> files1 = new ArrayList<String>();
-			files1.add("org.eclipse.core.resources.prefs");
-			addToProjectStructure(project, paths);
-			addFiles(project, null, Arrays.asList(files0));
-			addFiles(project, ".settings", files1);
-			addFiles(project, "data", files2);
+					"testSystem.system",
+					"testSystem.system_diagram",
+					"testSystem.usagemodel",
+					"testSystem.usagemodel_diagram" };
+			addFiles(project, null, Arrays.asList(filesArray));
+
+			// adding files to the .settings directory
+			List<String> filesList = new ArrayList<String>();
+			filesList.add("org.eclipse.core.resources.prefs");
+			addFiles(project, paths[0], filesList);
+
+			// adding files to the monitors directory
+			filesList.clear();
+			filesList.add("EvaluationSpecification.pms");
+			addFiles(project, paths[1], filesList);
+
+			// adding files to the reconv_qvto directory
+			filesList.clear();
+			filesList.add("scaleDown.qvto");
+			filesList.add("scaleUp.qvto");
+			addFiles(project, paths[2], filesList);
+
+			// adding files to the rules_active directory
+			filesList.clear();
+			filesList.add("outsource.qvto");
+			addFiles(project, paths[4], filesList);
+
+			// adding files to the rules_sd directory
+			filesList.clear();
+			filesList.add("acquireResources.sdm"); filesList.add("acquireResources.sdm_diagram");
+			filesList.add("releaseResources.sdm"); filesList.add("releaseResources.sdm_diagram");
+			addFiles(project, paths[5], filesList);
 		} catch (CoreException e) {
 			e.printStackTrace();
 			project = null;
@@ -84,28 +92,31 @@ public class CustomProjectSupport {
 		return project;
 	}
 
-	private static void addFiles(IProject project, String folderPath, List<String> files) {
-    	IContainer container = null;
-    	if(folderPath == null || "".equals(folderPath)){
-    		container = project;
-    	}else{
-    		container = project.getFolder(folderPath);
-    	}
-    	StringBuilder sourcePath = new StringBuilder("../structure/");
-    	if(folderPath != null && !"".equals(folderPath)){
-    		sourcePath.append(folderPath).append("/");
-    	}
-		for(String file : files){
-	    	InputStream inputStream = CustomProjectSupport.class.getClassLoader().getResourceAsStream(sourcePath.toString() + file);
-	    	IFile f = container.getFile(new Path(file));
-	        try {
+	private static void addFiles(IProject project, String folderPath,
+			List<String> files) {
+		IContainer container = null;
+		if (folderPath == null || "".equals(folderPath)) {
+			container = project;
+		} else {
+			container = project.getFolder(folderPath);
+		}
+		StringBuilder sourcePath = new StringBuilder("../structure/");
+		if (folderPath != null && !"".equals(folderPath)) {
+			sourcePath.append(folderPath).append("/");
+		}
+		for (String file : files) {
+			InputStream inputStream = CustomProjectSupport.class
+					.getClassLoader().getResourceAsStream(
+							sourcePath.toString() + file);
+			IFile f = container.getFile(new Path(file));
+			try {
 				f.create(inputStream, false, null);
 				inputStream.close();
-	        } catch (Exception e) {
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    	}
+		}
 	}
 
 	/**
@@ -161,24 +172,25 @@ public class CustomProjectSupport {
 	 * @param paths
 	 * @throws CoreException
 	 */
-	private static void addToProjectStructure(IProject newProject, String[] paths) throws CoreException {
+	private static void addToProjectStructure(IProject newProject,
+			String[] paths) throws CoreException {
 		for (String path : paths) {
 			IFolder etcFolders = newProject.getFolder(path);
 			createFolder(newProject, etcFolders);
 		}
 	}
-	
+
 	private static void addNature(IProject project) throws CoreException {
-        if (!project.hasNature(SimulizarNature.NATURE_ID)) {
-            IProjectDescription description = project.getDescription();
-            String[] prevNatures = description.getNatureIds();
-            String[] newNatures = new String[prevNatures.length + 1];
-            System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-            newNatures[prevNatures.length] = SimulizarNature.NATURE_ID;
-            description.setNatureIds(newNatures);
- 
-            IProgressMonitor monitor = null;
-            project.setDescription(description, monitor);
-        }
-    }
+		if (!project.hasNature(SimulizarNature.NATURE_ID)) {
+			IProjectDescription description = project.getDescription();
+			String[] prevNatures = description.getNatureIds();
+			String[] newNatures = new String[prevNatures.length + 1];
+			System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
+			newNatures[prevNatures.length] = SimulizarNature.NATURE_ID;
+			description.setNatureIds(newNatures);
+
+			IProgressMonitor monitor = null;
+			project.setDescription(description, monitor);
+		}
+	}
 }
