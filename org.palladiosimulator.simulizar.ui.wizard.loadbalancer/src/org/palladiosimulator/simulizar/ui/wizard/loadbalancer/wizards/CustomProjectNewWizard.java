d5e4bf18e57cd380d96bbb409877ca742ea896b6
igor_rogic <unknown.mail@emailaddr>
2014-04-02 07:49:43 +0000

    Putting the perspective plugin back and putting new plugins (znncom and loadbalancer) as the replacement for wizard.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@21582 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.simulizar.ui.wizard.loadbalancer/src/org/palladiosimulator/simulizar/ui/wizard/loadbalancer/wizards/CustomProjectNewWizard.java
@@ -0,0 +1,62 @@
+package org.palladiosimulator.simulizar.ui.wizard.loadbalancer.wizards;
+
+import java.net.URI;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+import org.palladiosimulator.simulizar.ui.wizard.loadbalancer.projects.CustomProjectSupport;
+
+public class CustomProjectNewWizard extends Wizard implements INewWizard, IExecutableExtension {
+
+	private WizardNewProjectCreationPage _pageOne;
+	private IConfigurationElement _configurationElement;
+	
+	public CustomProjectNewWizard() {
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean performFinish() {
+		String name = _pageOne.getProjectName();
+	    URI location = null;
+	    if (!_pageOne.useDefaults()) {
+	        location = _pageOne.getLocationURI();
+	    } // else location == null
+	 
+	    CustomProjectSupport.createProject(name, location);
+	 
+	    BasicNewProjectResourceWizard.updatePerspective(_configurationElement);
+	    
+	    return true;
+	}
+	
+	@Override
+	public void addPages() {
+	    super.addPages();
+	 
+	    _pageOne = new WizardNewProjectCreationPage("SimuLizar example project");
+	    _pageOne.setTitle("SimuLizar example project");
+	    _pageOne.setDescription("SimuLizar example project.");
+	 
+	    addPage(_pageOne);
+	}
+
+	@Override
+	public void setInitializationData(IConfigurationElement config,
+			String propertyName, Object data) throws CoreException {
+		_configurationElement = config;
+		
+	}
+
+}
