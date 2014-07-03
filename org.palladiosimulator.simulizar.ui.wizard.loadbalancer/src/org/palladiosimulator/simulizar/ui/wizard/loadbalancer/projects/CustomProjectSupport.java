3286451fb7216ef60ab3741453e3ed4481ceec86
matzebecker <matthias.becker@iem.fraunhofer.de>
2014-07-03 14:27:41 +0000

    New icons
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@23341 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.ui.wizard.loadbalancer/src/org/palladiosimulator/simulizar/ui/wizard/loadbalancer/projects/CustomProjectSupport.java
+++ org.palladiosimulator.simulizar.ui.wizard.loadbalancer/src/org/palladiosimulator/simulizar/ui/wizard/loadbalancer/projects/CustomProjectSupport.java
@@ -2,7 +2,6 @@ package org.palladiosimulator.simulizar.ui.wizard.loadbalancer.projects;
 
 import java.io.InputStream;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
