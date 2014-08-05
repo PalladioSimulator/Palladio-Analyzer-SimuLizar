e616041ff80e7cdee869a5727817936bfb61f409
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-08-05 11:25:32 +0000

    Refactored code: exchanged instanceof check with EMF sswitch implementation (preferred solution)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk/org.palladiosimulator.simulizar@23863 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- src/org/palladiosimulator/simulizar/utils/PMSUtil.java
+++ src/org/palladiosimulator/simulizar/utils/PMSUtil.java
@@ -4,12 +4,14 @@ import org.eclipse.emf.ecore.EObject;
 import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
 import org.palladiosimulator.edp2.models.measuringpoint.ResourceURIMeasuringPoint;
 import org.palladiosimulator.edp2.models.measuringpoint.StringMeasuringPoint;
+import org.palladiosimulator.edp2.models.measuringpoint.util.MeasuringpointSwitch;
 import org.palladiosimulator.pcmmeasuringpoint.ActiveResourceMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.AssemblyOperationMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.AssemblyPassiveResourceMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.SubSystemOperationMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.SystemOperationMeasuringPoint;
 import org.palladiosimulator.pcmmeasuringpoint.UsageScenarioMeasuringPoint;
+import org.palladiosimulator.pcmmeasuringpoint.util.PcmmeasuringpointSwitch;
 import org.palladiosimulator.simulizar.pms.MeasurementSpecification;
 import org.palladiosimulator.simulizar.pms.PMSModel;
 import org.palladiosimulator.simulizar.pms.PerformanceMeasurement;
@@ -52,64 +54,108 @@ public final class PMSUtil {
         return null;
     }
 
-    /**
-     * TODO Really ugly, hacked code ;) Refactor into measuring point objects' class? [Lehrig]
-     */
     private static boolean elementConformingToMeasuringPoint(final EObject element, final MeasuringPoint measuringPoint) {
         if (measuringPoint == null) {
             throw new IllegalArgumentException("Measuring point cannot be null");
-        } else if (measuringPoint instanceof ResourceURIMeasuringPoint) {
-            final String measuringPointResourceURI = ((ResourceURIMeasuringPoint) measuringPoint).getResourceURI();
-            final String elementResourceFragment = ModelsAtRuntime.getResourceFragment(element);
-            return measuringPointResourceURI.endsWith(elementResourceFragment);
-        } else if (measuringPoint instanceof StringMeasuringPoint) {
-            throw new IllegalArgumentException("String measuring points are forbidden for SimuLizar");
-        } else if (measuringPoint instanceof ActiveResourceMeasuringPoint) {
-            ProcessingResourceSpecification activeResource = ((ActiveResourceMeasuringPoint) measuringPoint)
-                    .getActiveResource();
-            if (element instanceof ResourceContainer) {
-                ResourceContainer resourceContainer = (ResourceContainer) element;
-                return resourceContainer.getId().equals(
-                        activeResource.getResourceContainer_ProcessingResourceSpecification().getId());
-            } else {
-                throw new IllegalArgumentException(
-                        "ResourceContainer is the only supported ActiveResourceMeasuringPoint");
-            }
-        } else if (measuringPoint instanceof AssemblyOperationMeasuringPoint) {
-            AssemblyOperationMeasuringPoint mP = (AssemblyOperationMeasuringPoint) measuringPoint;
-            if (element instanceof ExternalCallAction) {
-                ExternalCallAction externalCallAction = (ExternalCallAction) element;
-                return externalCallAction.getCalledService_ExternalService().getId()
-                        .equals(mP.getOperationSignature().getId())
-                        && externalCallAction.getRole_ExternalService().getId().equals(mP.getRole().getId());
-            } else {
-                throw new IllegalArgumentException("Unsupported element for a AssemblyOperationMeasuringPoint");
-            }
-        } else if (measuringPoint instanceof AssemblyPassiveResourceMeasuringPoint) {
-            throw new IllegalArgumentException("Passive resources are currently unsupported by SimuLizar");
-        } else if (measuringPoint instanceof SubSystemOperationMeasuringPoint) {
-            throw new IllegalArgumentException("Subsystems are currently unsupported by SimuLizar");
-        } else if (measuringPoint instanceof SystemOperationMeasuringPoint) {
-            SystemOperationMeasuringPoint mP = (SystemOperationMeasuringPoint) measuringPoint;
-            if (element instanceof EntryLevelSystemCall) {
-                EntryLevelSystemCall entryLevelSystemCall = (EntryLevelSystemCall) element;
-                return entryLevelSystemCall.getOperationSignature__EntryLevelSystemCall().getId()
-                        .equals(mP.getOperationSignature().getId())
-                        && entryLevelSystemCall.getProvidedRole_EntryLevelSystemCall().getId()
-                                .equals(mP.getRole().getId());
-            } else {
-                throw new IllegalArgumentException("Unsupported element for a SystemOperationMeasuringPoint");
-            }
-        } else if (measuringPoint instanceof UsageScenarioMeasuringPoint) {
-            UsageScenarioMeasuringPoint mP = (UsageScenarioMeasuringPoint) measuringPoint;
-            if (element instanceof UsageScenario) {
-                UsageScenario usageScenario = (UsageScenario) element;
-                return usageScenario.getId().equals(mP.getUsageScenario().getId());
-            }
-        } else {
-            throw new IllegalArgumentException("Unknown measuring point type");
         }
-        return false;
+
+        Boolean result = checkGeneralMeasuringPoints(element, measuringPoint);
+
+        if (result == null) {
+            result = checkPCMMeasuringPoints(element, measuringPoint);
+
+            if (result == null) {
+                throw new IllegalArgumentException("Unknown measuring point type");
+            }
+        }
+
+        return result.booleanValue();
+    }
+
+    private static Boolean checkPCMMeasuringPoints(final EObject element, final MeasuringPoint measuringPoint) {
+        return new PcmmeasuringpointSwitch<Boolean>() {
+
+            @Override
+            public Boolean caseActiveResourceMeasuringPoint(ActiveResourceMeasuringPoint object) {
+                final ProcessingResourceSpecification activeResource = object.getActiveResource();
+                if (element instanceof ResourceContainer) {
+                    ResourceContainer resourceContainer = (ResourceContainer) element;
+                    return resourceContainer.getId().equals(
+                            activeResource.getResourceContainer_ProcessingResourceSpecification().getId());
+                } else {
+                    throw new IllegalArgumentException(
+                            "ResourceContainer is the only supported ActiveResourceMeasuringPoint");
+                }
+            }
+
+            @Override
+            public Boolean caseAssemblyOperationMeasuringPoint(AssemblyOperationMeasuringPoint object) {
+                if (element instanceof ExternalCallAction) {
+                    final ExternalCallAction externalCallAction = (ExternalCallAction) element;
+                    return externalCallAction.getCalledService_ExternalService().getId()
+                            .equals(object.getOperationSignature().getId())
+                            && externalCallAction.getRole_ExternalService().getId().equals(object.getRole().getId());
+                } else {
+                    throw new IllegalArgumentException("Unsupported element for a AssemblyOperationMeasuringPoint");
+                }
+            }
+
+            @Override
+            public Boolean caseAssemblyPassiveResourceMeasuringPoint(AssemblyPassiveResourceMeasuringPoint object) {
+                throw new IllegalArgumentException("Passive resources are currently unsupported by SimuLizar");
+            }
+
+            @Override
+            public Boolean caseSubSystemOperationMeasuringPoint(SubSystemOperationMeasuringPoint object) {
+                throw new IllegalArgumentException("Subsystems are currently unsupported by SimuLizar");
+            }
+
+            @Override
+            public Boolean caseSystemOperationMeasuringPoint(SystemOperationMeasuringPoint object) {
+                if (element instanceof EntryLevelSystemCall) {
+                    EntryLevelSystemCall entryLevelSystemCall = (EntryLevelSystemCall) element;
+                    return entryLevelSystemCall.getOperationSignature__EntryLevelSystemCall().getId()
+                            .equals(object.getOperationSignature().getId())
+                            && entryLevelSystemCall.getProvidedRole_EntryLevelSystemCall().getId()
+                                    .equals(object.getRole().getId());
+                } else {
+                    throw new IllegalArgumentException("Unsupported element for a SystemOperationMeasuringPoint");
+                }
+            }
+
+            @Override
+            public Boolean caseUsageScenarioMeasuringPoint(UsageScenarioMeasuringPoint object) {
+                if (element instanceof UsageScenario) {
+                    UsageScenario usageScenario = (UsageScenario) element;
+                    return usageScenario.getId().equals(object.getUsageScenario().getId());
+                }
+
+                throw new IllegalArgumentException("Unknown UsageScenarioMeasuringPoint");
+            }
+
+        }
+
+        .doSwitch(measuringPoint);
+    }
+
+    private static Boolean checkGeneralMeasuringPoints(final EObject element, final MeasuringPoint measuringPoint) {
+        return new MeasuringpointSwitch<Boolean>() {
+
+            @Override
+            public Boolean caseResourceURIMeasuringPoint(ResourceURIMeasuringPoint object) {
+                final String measuringPointResourceURI = object.getResourceURI();
+                final String elementResourceFragment = ModelsAtRuntime.getResourceFragment(element);
+                return measuringPointResourceURI.endsWith(elementResourceFragment);
+            }
+
+            @Override
+            public Boolean caseStringMeasuringPoint(StringMeasuringPoint object) {
+                throw new IllegalArgumentException("String measuring points are forbidden for SimuLizar");
+            };
+
+        }
+
+        .doSwitch(measuringPoint);
     }
 
 }
