0d4b35339feaa19b8104573de6edb4ffc99b2cdf
erlendstav <erlend.stav@sintef.no>
2014-09-18 13:58:11 +0000

    Added support for UsageEvolution in SimuLizar
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk/org.palladiosimulator.simulizar@24571 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- src/org/palladiosimulator/simulizar/runconfig/SimuLizarWorkflowConfiguration.java
+++ src/org/palladiosimulator/simulizar/runconfig/SimuLizarWorkflowConfiguration.java
@@ -8,6 +8,7 @@ public class SimuLizarWorkflowConfiguration extends SimuComWorkflowConfiguration
 
     protected String pmsFile;
     protected String reconfigurationRulesFolder;
+    protected String usageEvolutionFile;
 
     public SimuLizarWorkflowConfiguration(Map<String, Object> attributes) {
         super(attributes);
@@ -29,6 +30,14 @@ public class SimuLizarWorkflowConfiguration extends SimuComWorkflowConfiguration
         this.reconfigurationRulesFolder = reconfigurationRulesFolder;
     }
 
+    public String getUsageEvolutionFile() {
+        return usageEvolutionFile;
+    }
+
+    public void setUsageEvolutionFile(String usageEvolutionFile) {
+        this.usageEvolutionFile = usageEvolutionFile;
+    }
+
     @Override
     public void setDefaults() {
         throw new RuntimeException("Not implemented. No defaults defined.");
@@ -39,6 +48,7 @@ public class SimuLizarWorkflowConfiguration extends SimuComWorkflowConfiguration
         SimuLizarWorkflowConfiguration config = (SimuLizarWorkflowConfiguration) super.clone();
         config.pmsFile = this.pmsFile;
         config.reconfigurationRulesFolder = this.reconfigurationRulesFolder;
+        config.usageEvolutionFile = this.usageEvolutionFile;
         return config;
     }
 
