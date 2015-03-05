5d9e48c699d8e2a062d152363dde729d3e194137
florian-rosenthal <florian.rosenthal@kit.edu>
2015-03-05 15:32:53 +0000

    remove argument of callback method, as value can be retrieved via getter of corresponding scope
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches/SlidingWindow@25592 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/metrics/powerconsumption/ISimulationEvaluationScopeListener.java
+++ org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/metrics/powerconsumption/ISimulationEvaluationScopeListener.java
@@ -1,8 +1,6 @@
 package org.palladiosimulator.simulizar.metrics.powerconsumption;
 
-import javax.measure.Measure;
-import javax.measure.quantity.Duration;
 
 public interface ISimulationEvaluationScopeListener {
-    public void next(Measure<Double, Duration> currentPointInTime);
+    public void newElementAvailable();
 }
