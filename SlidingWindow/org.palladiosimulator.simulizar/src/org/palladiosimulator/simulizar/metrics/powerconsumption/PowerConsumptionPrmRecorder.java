52788ecd175611ed8ae2d2b692cd2cf556efc760
florian-rosenthal <florian.rosenthal@kit.edu>
2015-03-24 10:21:24 +0000

    adaptations due to renaming of PMS
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@25746 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SlidingWindow/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/metrics/powerconsumption/PowerConsumptionPrmRecorder.java
+++ SlidingWindow/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/metrics/powerconsumption/PowerConsumptionPrmRecorder.java
@@ -7,27 +7,47 @@ import org.palladiosimulator.measurementframework.MeasuringValue;
 import org.palladiosimulator.measurementframework.listener.IMeasurementSourceListener;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.simulizar.metrics.PRMRecorder;
-import org.palladiosimulator.simulizar.pms.MeasurementSpecification;
-import org.palladiosimulator.simulizar.pms.PerformanceMetricEnum;
+import org.palladiosimulator.simulizar.monitorrepository.MeasurementSpecification;
+import org.palladiosimulator.simulizar.monitorrepository.MonitorRepository;
 import org.palladiosimulator.simulizar.prm.PCMModelElementMeasurement;
 import org.palladiosimulator.simulizar.prm.PRMModel;
 
 import de.fzi.power.infrastructure.PowerProvidingEntity;
 
+/**
+ * This class is responsible for propagating {@link MeasurementSpecification}s related to power consumption to the PRM.<br>
+ * Being as this class implements the {@link IMeasurementSourceListener} interface, instances it can be attached to any object
+ * that produces {@link MeasuringValue}s which adhere to the {@link MetricDescriptionConstants#POWER_CONSUMPTION_TUPLE} metric. 
+ * @author Florian Rosenthal
+ *
+ */
 public class PowerConsumptionPrmRecorder extends PRMRecorder implements IMeasurementSourceListener {
 
+    /**
+     * Initializes a new instance of the {@link PowerConsumptionPrmRecorder} class with the given arguments.
+     * @param prmAccess The {@link PRMModel} model instance the power consumption measurements shall be forwarded to.
+     * @param measurementSpecification The {@link MeasurementSpecification} as defined in a {@link MonitorRepository}.  
+     * @param monitoredElement The {@link PowerProvidingEntity} that is monitored by the given {@code measurementSpecification}.
+     */
     public PowerConsumptionPrmRecorder(PRMModel prmAccess, MeasurementSpecification measurementSpecification,
             PowerProvidingEntity monitoredElement) {
         super(prmAccess, measurementSpecification, monitoredElement);
         if (prmAccess == null || measurementSpecification == null || monitoredElement == null) {
             throw new IllegalArgumentException("At least one argument is null.");
         }
-        if (measurementSpecification.getPerformanceMetric() != PerformanceMetricEnum.POWER_CONSUMPTION) {
-            throw new IllegalArgumentException("Metric of given MeasurementSpecification instance must be power consumption!");
+        if (!measurementSpecification.getMetricDescription().equals(MetricDescriptionConstants.POWER_CONSUMPTION_TUPLE)) {
+            throw new IllegalArgumentException("Metric of given MeasurementSpecification instance must be "
+                    + MetricDescriptionConstants.POWER_CONSUMPTION_TUPLE.getName() + "!");
         }
         addToPRM(getPCMModelElementMeasurement());
     }
 
+    /**{@inheritDoc}<br>
+     * When this method is called by the observed subject, the given {@link MeasuringValue} is propagated 
+     * to the associated {@link PRMModel} instance.
+     * @throws IllegalArgumentException In case the passed {@link MeasuringValue} is not compatible with 
+     * the {@link MetricDescriptionConstants#POWER_CONSUMPTION_TUPLE} metric.
+     */
     @Override
     public void newMeasurementAvailable(MeasuringValue newMeasurement) {
         if (newMeasurement == null || !newMeasurement.isCompatibleWith(MetricDescriptionConstants.POWER_CONSUMPTION_TUPLE)) {
