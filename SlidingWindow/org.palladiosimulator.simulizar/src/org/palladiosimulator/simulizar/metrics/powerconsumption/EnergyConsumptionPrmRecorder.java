185c6a932f5cb7f5dfcc9725a4e38925ec70e0ff
florian-rosenthal <florian.rosenthal@kit.edu>
2015-03-25 15:17:44 +0000

    slight adaptations due to changes in base class PRMRecorder
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@25787 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- SlidingWindow/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/metrics/powerconsumption/EnergyConsumptionPrmRecorder.java
+++ SlidingWindow/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/metrics/powerconsumption/EnergyConsumptionPrmRecorder.java
@@ -3,32 +3,43 @@ package org.palladiosimulator.simulizar.metrics.powerconsumption;
 import javax.measure.Measure;
 import javax.measure.quantity.Energy;
 
+import org.palladiosimulator.edp2.util.MetricDescriptionUtility;
 import org.palladiosimulator.measurementframework.MeasuringValue;
 import org.palladiosimulator.measurementframework.listener.IMeasurementSourceListener;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.simulizar.metrics.PRMRecorder;
 import org.palladiosimulator.simulizar.monitorrepository.MeasurementSpecification;
 import org.palladiosimulator.simulizar.monitorrepository.MonitorRepository;
-import org.palladiosimulator.simulizar.prm.PCMModelElementMeasurement;
 import org.palladiosimulator.simulizar.prm.PRMModel;
 
 import de.fzi.power.infrastructure.PowerProvidingEntity;
 
 /**
- * This class is responsible for propagating {@link MeasurementSpecification}s related to power consumption to the PRM.<br>
- * Being as this class implements the {@link IMeasurementSourceListener} interface, instances it can be attached to any object
- * that produces {@link MeasuringValue}s which adhere to the 
- * {@link MetricDescriptionConstants#CUMULATIVE_ENERGY_CONSUMPTION_TUPLE} metric. 
+ * This class is responsible for propagating {@link MeasurementSpecification}s related to power
+ * consumption to the PRM.<br>
+ * Being as this class implements the {@link IMeasurementSourceListener} interface, instances can
+ * be attached to any object that produces {@link MeasuringValue}s which adhere to the
+ * {@link MetricDescriptionConstants#CUMULATIVE_ENERGY_CONSUMPTION_TUPLE} metric.
+ * 
  * @author Florian Rosenthal
  *
  */
 public class EnergyConsumptionPrmRecorder extends PRMRecorder implements IMeasurementSourceListener {
 
     /**
-     * Initializes a new instance of the {@link EnergyConsumptionPrmRecorder} class with the given arguments.
-     * @param prmAccess The {@link PRMModel} model instance the power consumption measurements shall be forwarded to.
-     * @param measurementSpecification The {@link MeasurementSpecification} as defined in a {@link MonitorRepository}.  
-     * @param monitoredElement The {@link PowerProvidingEntity} that is monitored by the given {@code measurementSpecification}.
+     * Initializes a new instance of the {@link EnergyConsumptionPrmRecorder} class with the given
+     * arguments.
+     * 
+     * @param prmAccess
+     *            The {@link PRMModel} model instance the power consumption measurements shall be
+     *            forwarded to.
+     * @param measurementSpecification
+     *            The {@link MeasurementSpecification} as defined in a {@link MonitorRepository}.
+     * @param monitoredElement
+     *            The {@link PowerProvidingEntity} that is monitored by the given
+     *            {@code measurementSpecification}.
+     * @throws IllegalArgumentException
+     *             In case one of the arguments is {@code null}.
      */
     public EnergyConsumptionPrmRecorder(PRMModel prmAccess, MeasurementSpecification measurementSpecification,
             PowerProvidingEntity monitoredElement) {
@@ -37,12 +48,11 @@ public class EnergyConsumptionPrmRecorder extends PRMRecorder implements IMeasur
         if (prmAccess == null || measurementSpecification == null || monitoredElement == null) {
             throw new IllegalArgumentException("At least one argument is null.");
         }
-        if (!measurementSpecification.getMetricDescription().equals(
+        if (!MetricDescriptionUtility.metricDescriptionIdsEqual(measurementSpecification.getMetricDescription(),
                 MetricDescriptionConstants.CUMULATIVE_ENERGY_CONSUMPTION_TUPLE)) {
             throw new IllegalArgumentException("Metric of given MeasurementSpecification instance must be "
                     + MetricDescriptionConstants.CUMULATIVE_ENERGY_CONSUMPTION_TUPLE.getName() + "!");
         }
-        addToPRM(getPCMModelElementMeasurement());
     }
 
     /**
@@ -63,35 +73,12 @@ public class EnergyConsumptionPrmRecorder extends PRMRecorder implements IMeasur
         Measure<Double, Energy> energyMeasure = newMeasurement
                 .getMeasureForMetric(MetricDescriptionConstants.ENERGY_CONSUMPTION);
         // forward power value (expressed as double in receiving unit!) to PRM
-        setMeasurementValue(energyMeasure.doubleValue(energyMeasure.getUnit()));
+        updateMeasurementValue(energyMeasure.doubleValue(energyMeasure.getUnit()));
     }
 
     @Override
     public void preUnregister() {
-        removeFromPRM(getPCMModelElementMeasurement());
+        detachFromPRM();
     }
 
-    @Override
-    protected final void addToPRM(double newValue) {
-        // argument value is ignored
-        addToPRM(getPCMModelElementMeasurement());
-    }
-
-    private void addToPRM(PCMModelElementMeasurement pcmModelElementMeasurement) {
-        assert pcmModelElementMeasurement != null;
-        if (!getPrmModel().getPcmModelElementMeasurements().contains(pcmModelElementMeasurement)) {
-            getPrmModel().getPcmModelElementMeasurements().add(pcmModelElementMeasurement);
-        }
-    }
-
-    private void removeFromPRM(PCMModelElementMeasurement pcmModelElementMeasurement) {
-        assert pcmModelElementMeasurement != null;
-        getPrmModel().getPcmModelElementMeasurements().remove(pcmModelElementMeasurement);
-    }
-
-    private void setMeasurementValue(double newValue) {
-        assert !Double.isInfinite(newValue) && !Double.isNaN(newValue);
-
-        super.getPCMModelElementMeasurement().setMeasurementValue(newValue);
-    }
 }
