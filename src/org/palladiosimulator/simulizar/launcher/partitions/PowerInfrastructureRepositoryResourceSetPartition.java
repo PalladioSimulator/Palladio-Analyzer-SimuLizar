f2ce56b2c20dea9e47850c3499a35512b2818b25
florian-rosenthal <florian.rosenthal@kit.edu>
2015-04-30 15:07:27 +0000

    add blackboard ResourceSetPartition for PowerInfraStructureRepository (aka Power Infrastructure Model)
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk/org.palladiosimulator.simulizar@26343 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ src/org/palladiosimulator/simulizar/launcher/partitions/PowerInfrastructureRepositoryResourceSetPartition.java
@@ -0,0 +1,40 @@
+package org.palladiosimulator.simulizar.launcher.partitions;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import de.fzi.power.infrastructure.InfrastructurePackage;
+import de.fzi.power.infrastructure.PowerInfrastructureRepository;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
+
+public final class PowerInfrastructureRepositoryResourceSetPartition extends ResourceSetPartition {
+
+    private static final Logger LOGGER = Logger.getLogger(PowerInfrastructureRepositoryResourceSetPartition.class);
+    private PowerInfrastructureRepository powerInfrastructureRepositoryModel;
+
+    public PowerInfrastructureRepositoryResourceSetPartition() {
+    }
+
+    public PowerInfrastructureRepository getPowerInfrastructureRepositoryModel() {
+        if (this.powerInfrastructureRepositoryModel == null) {
+            this.powerInfrastructureRepositoryModel = loadPowerInfrastructureRepositoryModel();
+        }
+        return this.powerInfrastructureRepositoryModel;
+    }
+
+    /**
+     * @return return the MonitorRepository element
+     */
+    private PowerInfrastructureRepository loadPowerInfrastructureRepositoryModel() {
+        try {
+            LOGGER.debug("Retrieving Power Infrastructure Repository Model from blackboard partition");
+            List<PowerInfrastructureRepository> result = getElement(InfrastructurePackage.eINSTANCE
+                    .getPowerInfrastructureRepository());
+            return result.get(0);
+        } catch (Exception e) {
+            LOGGER.warn("No Infrastructure Repository found, no requests will be measured.");
+            return null;
+        }
+    }
+}
