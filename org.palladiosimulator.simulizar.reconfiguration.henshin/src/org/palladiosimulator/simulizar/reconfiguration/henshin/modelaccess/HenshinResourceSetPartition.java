1176e7a73c4c3aa92734390fe31269b8e2a64f9b
matzebecker <matthias.becker@iem.fraunhofer.de>
2015-05-04 14:42:28 +0000

    Moved Henshin model loading from SimuLizar Core to reconfiguration engine plugin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@26385 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.simulizar.reconfiguration.henshin/src/org/palladiosimulator/simulizar/reconfiguration/henshin/modelaccess/HenshinResourceSetPartition.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.palladiosimulator.simulizar.reconfiguration.henshin.modelaccess;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.henshin.model.Module;
+
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
+
+/**
+ * @author Matthias Becker
+ *
+ */
+public class HenshinResourceSetPartition extends ResourceSetPartition {
+
+    private List<Module> createModules(String path) {
+        /*
+         * 
+         * final File folder = FileUtil.getFolder(path); final File[] files =
+         * FileUtil.getFiles(folder, HENSHIN_FILE_EXTENSION);
+         * 
+         * List<Module> modules = new LinkedList<Module>(); if (files != null && files.length > 0) {
+         * for (final File file : files) {
+         * modules.add(this.henshinResourceSet.getModule(file.getPath(), false)); } } return
+         * modules;
+         */
+        List<Module> modules = new Vector<Module>();
+        for (final Resource resource : this.rs.getResources()) {
+            Module module = null;
+
+            if (resource != null) {
+                for (EObject object : resource.getContents()) {
+                    if (object instanceof Module) {
+                        module = (Module) object;
+                        break;
+                    }
+                }
+            }
+            modules.add(module);
+        }
+        return modules;
+    }
+
+}
