22fab4617c645befb2273230e9f0b662768c4d6d
matzebecker <matthias.becker@iem.fraunhofer.de>
2015-05-07 07:37:47 +0000

    Implemented first draft of automatic SLO evaluation job
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@26419 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.ui/src/org/palladiosimulator/simulizar/ui/configuration/InterpreterFileNamesInputTab.java
+++ org.palladiosimulator.simulizar.ui/src/org/palladiosimulator/simulizar/ui/configuration/InterpreterFileNamesInputTab.java
@@ -25,10 +25,12 @@ public class InterpreterFileNamesInputTab extends ProtocomFileNamesInputTab {
     protected Text reconfigurationRulesFolder;
     /** Text field for path to usage evolution file. */
     protected Text usageEvolutionFile;
-    
+    /** Text field for service level objectives file. */
+    protected Text serviceLevelObjectivesFile;
+
     protected Text infrastructureModelFileText;
 
-     /**
+    /**
      * @see de.uka.ipd.sdq.workflow.launchconfig.tabs.FileNamesInputTab#createControl(org.eclipse.swt.widgets.Composite)
      */
     @Override
@@ -47,9 +49,18 @@ public class InterpreterFileNamesInputTab extends ProtocomFileNamesInputTab {
          * Create reconfiguration rules folder section
          */
         reconfigurationRulesFolder = new Text(container, SWT.SINGLE | SWT.BORDER);
-        TabHelper.createFolderInputSection(container, modifyListener, "Optional: Reconfiguration Rules folder ",
+        TabHelper.createFolderInputSection(container, modifyListener, "Optional: Reconfiguration Rules folder",
                 reconfigurationRulesFolder, "Select Reconfiguration Rules Folder", getShell(),
                 SimulizarConstants.DEFAULT_RECONFIGURATION_RULES_FOLDER);
+
+        /**
+         * Create service level objective repository section
+         */
+        serviceLevelObjectivesFile = new Text(container, SWT.SINGLE | SWT.BORDER);
+        TabHelper.createFileInputSection(container, modifyListener, "Optional: Service Level Objectives File",
+                SimulizarConstants.SERVICELEVELOBJECTIVEREPOSITORY_FILE_EXTENSION, serviceLevelObjectivesFile,
+                "Select Service Level Objective Repository File", getShell(),
+                SimulizarConstants.SERVICELEVELOBJECTIVEREPOSITORY_FILE);
 
         /**
          * Create UsageEvolution file section
@@ -58,39 +69,45 @@ public class InterpreterFileNamesInputTab extends ProtocomFileNamesInputTab {
         TabHelper.createFileInputSection(container, modifyListener, "Optional: Usage Evolution File",
                 SimulizarConstants.USAGEEVOLUTION_FILE_EXTENSION, usageEvolutionFile, "Select Usage Evolution File",
                 getShell(), SimulizarConstants.DEFAULT_USAGEEVOLUTION_FILE);
-        
+
         /**
          * Create Infrastructure model file section
          */
         this.infrastructureModelFileText = new Text(container, SWT.SINGLE | SWT.BORDER);
-        TabHelper.createFileInputSection(container, modifyListener, "Optional: Infrastructure Model File (For Power Analyses)",
-                SimulizarConstants.INFRASTRUCTURE_MODEL_FILE_EXTENSIONS, this.infrastructureModelFileText, "Select Infrastructure Model File",
-                getShell(), SimulizarConstants.DEFAULT_INFRASTRUCTURE_MODEL_FILE);
+        TabHelper.createFileInputSection(container, modifyListener,
+                "Optional: Infrastructure Model File (For Power Analyses)",
+                SimulizarConstants.INFRASTRUCTURE_MODEL_FILE_EXTENSIONS, this.infrastructureModelFileText,
+                "Select Infrastructure Model File", getShell(), SimulizarConstants.DEFAULT_INFRASTRUCTURE_MODEL_FILE);
 
     }
 
-    private void setTextFromConfigAttribute(Text textWidget, ILaunchConfiguration config, String attributeName, String defaultValue) {
+    private void setTextFromConfigAttribute(Text textWidget, ILaunchConfiguration config, String attributeName,
+            String defaultValue) {
         try {
             textWidget.setText(config.getAttribute(attributeName, defaultValue));
         } catch (CoreException e) {
             LaunchConfigPlugin.errorLogger(getName(), attributeName, e.getMessage());
         }
     }
-    
+
     /**
      * @see de.uka.ipd.sdq.workflow.launchconfig.tabs.FileNamesInputTab#initializeFrom(org.eclipse.debug.core.ILaunchConfiguration)
      */
     @Override
     public void initializeFrom(final ILaunchConfiguration configuration) {
         super.initializeFrom(configuration);
-        setTextFromConfigAttribute(this.monitorRepositoryFile, configuration, SimulizarConstants.MONITOR_REPOSITORY_FILE, 
-                SimulizarConstants.DEFAULT_MONITOR_REPOSITORY_FILE);
+        setTextFromConfigAttribute(this.monitorRepositoryFile, configuration,
+                SimulizarConstants.MONITOR_REPOSITORY_FILE, SimulizarConstants.DEFAULT_MONITOR_REPOSITORY_FILE);
         setTextFromConfigAttribute(this.reconfigurationRulesFolder, configuration,
-                SimulizarConstants.RECONFIGURATION_RULES_FOLDER, SimulizarConstants.DEFAULT_RECONFIGURATION_RULES_FOLDER);
+                SimulizarConstants.RECONFIGURATION_RULES_FOLDER,
+                SimulizarConstants.DEFAULT_RECONFIGURATION_RULES_FOLDER);
+        setTextFromConfigAttribute(this.serviceLevelObjectivesFile, configuration,
+                SimulizarConstants.SERVICELEVELOBJECTIVEREPOSITORY_FILE,
+                SimulizarConstants.DEFAULT_SERVICELEVELOBJECTIVE_FILE);
         setTextFromConfigAttribute(this.usageEvolutionFile, configuration, SimulizarConstants.USAGEEVOLUTION_FILE,
                 SimulizarConstants.DEFAULT_USAGEEVOLUTION_FILE);
-        setTextFromConfigAttribute(this.infrastructureModelFileText, configuration, SimulizarConstants.INFRASTRUCTURE_MODEL_FILE,
-                SimulizarConstants.DEFAULT_INFRASTRUCTURE_MODEL_FILE);
+        setTextFromConfigAttribute(this.infrastructureModelFileText, configuration,
+                SimulizarConstants.INFRASTRUCTURE_MODEL_FILE, SimulizarConstants.DEFAULT_INFRASTRUCTURE_MODEL_FILE);
     }
 
     /*
@@ -105,8 +122,11 @@ public class InterpreterFileNamesInputTab extends ProtocomFileNamesInputTab {
         configuration.setAttribute(SimulizarConstants.MONITOR_REPOSITORY_FILE, monitorRepositoryFile.getText());
         configuration.setAttribute(SimulizarConstants.RECONFIGURATION_RULES_FOLDER,
                 reconfigurationRulesFolder.getText());
+        configuration.setAttribute(SimulizarConstants.SERVICELEVELOBJECTIVEREPOSITORY_FILE,
+                serviceLevelObjectivesFile.getText());
         configuration.setAttribute(SimulizarConstants.USAGEEVOLUTION_FILE, usageEvolutionFile.getText());
-        configuration.setAttribute(SimulizarConstants.INFRASTRUCTURE_MODEL_FILE, this.infrastructureModelFileText.getText());
+        configuration.setAttribute(SimulizarConstants.INFRASTRUCTURE_MODEL_FILE,
+                this.infrastructureModelFileText.getText());
     }
 
     /*
@@ -122,6 +142,8 @@ public class InterpreterFileNamesInputTab extends ProtocomFileNamesInputTab {
                 SimulizarConstants.DEFAULT_MONITOR_REPOSITORY_FILE);
         configuration.setAttribute(SimulizarConstants.RECONFIGURATION_RULES_FOLDER,
                 SimulizarConstants.DEFAULT_RECONFIGURATION_RULES_FOLDER);
+        configuration.setAttribute(SimulizarConstants.SERVICELEVELOBJECTIVEREPOSITORY_FILE,
+                SimulizarConstants.DEFAULT_SERVICELEVELOBJECTIVE_FILE);
         configuration.setAttribute(SimulizarConstants.USAGEEVOLUTION_FILE,
                 SimulizarConstants.DEFAULT_USAGEEVOLUTION_FILE);
         configuration.setAttribute(SimulizarConstants.INFRASTRUCTURE_MODEL_FILE,
