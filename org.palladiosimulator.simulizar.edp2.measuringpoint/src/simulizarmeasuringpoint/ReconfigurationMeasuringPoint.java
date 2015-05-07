6456bf9b7a938d13ecb118d3b4177c45604c7fe7
matzebecker <matthias.becker@iem.fraunhofer.de>
2015-05-07 11:04:00 +0000

    Share project "org.palladiosimulator.simulizar.edp2.measuringpoint" into "https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar" Share project "org.palladiosimulator.simulizar.edp2.measuringpoint.edit" into "https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar"
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@26430 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.simulizar.edp2.measuringpoint/src/simulizarmeasuringpoint/ReconfigurationMeasuringPoint.java
@@ -0,0 +1,17 @@
+/**
+ */
+package simulizarmeasuringpoint;
+
+import org.palladiosimulator.edp2.models.measuringpoint.StringMeasuringPoint;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object '
+ * <em><b>Reconfiguration Measuring Point</b></em>'. <!-- end-user-doc -->
+ *
+ *
+ * @see simulizarmeasuringpoint.SimulizarmeasuringpointPackage#getReconfigurationMeasuringPoint()
+ * @model
+ * @generated
+ */
+public interface ReconfigurationMeasuringPoint extends StringMeasuringPoint {
+} // ReconfigurationMeasuringPoint
