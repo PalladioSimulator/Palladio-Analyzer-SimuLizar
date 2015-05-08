4752a48470f751f1ed06b54dadfc89087c470403
florian-rosenthal <florian.rosenthal@kit.edu>
2015-05-08 15:41:57 +0000

    adapt PCMInterpreterRootCompositeJob so that additional model load jobs (i.e., extensions) are now added prior to starting the simulation
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@26448 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/launcher/jobs/PCMInterpreterRootCompositeJob.java
+++ org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/launcher/jobs/PCMInterpreterRootCompositeJob.java
@@ -1,7 +1,11 @@
 package org.palladiosimulator.simulizar.launcher.jobs;
 
+import org.palladiosimulator.commons.eclipseutils.ExtensionHelper;
+import org.palladiosimulator.simulizar.launcher.SimulizarConstants;
 import org.palladiosimulator.simulizar.runconfig.SimuLizarWorkflowConfiguration;
 
+import de.uka.ipd.sdq.workflow.extension.AbstractWorkflowExtensionConfigurationBuilder;
+import de.uka.ipd.sdq.workflow.extension.AbstractWorkflowExtensionJob;
 import de.uka.ipd.sdq.workflow.jobs.IBlackboardInteractingJob;
 import de.uka.ipd.sdq.workflow.jobs.SequentialBlackboardInteractingJob;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
@@ -33,7 +37,7 @@ public class PCMInterpreterRootCompositeJob extends SequentialBlackboardInteract
 
         this.addJob(new LoadUEModelIntoBlackboardJob(configuration));
 
-        // this.addJob(new LoadSDMModelsIntoBlackboardJob(configuration));
+        addModelLoadExtensionJobs(configuration);
 
         this.addJob(new PCMStartInterpretationJob(configuration));
 
@@ -41,4 +45,25 @@ public class PCMInterpreterRootCompositeJob extends SequentialBlackboardInteract
 
     }
 
+    private void addModelLoadExtensionJobs(SimuLizarWorkflowConfiguration configuration) {
+        Iterable<AbstractWorkflowExtensionJob<MDSDBlackboard>> loadJobs = ExtensionHelper.getExecutableExtensions(
+                SimulizarConstants.MODEL_LOAD_EXTENSION_POINT_ID,
+                SimulizarConstants.MODEL_LOAD_EXTENSION_POINT_JOB_ATTRIBUTE,
+                SimulizarConstants.MODEL_LOAD_EXTENSION_POINT_JOB_ATTRIBUTE);
+
+        for (AbstractWorkflowExtensionJob<MDSDBlackboard> loadJob : loadJobs) {
+            // check if corresponding config builder is available
+            // filter available extensions by name of job class
+            // this can be improved
+            AbstractWorkflowExtensionConfigurationBuilder builder = ExtensionHelper.getExecutableExtension(
+                    SimulizarConstants.MODEL_LOAD_EXTENSION_POINT_ID,
+                    SimulizarConstants.MODEL_LOAD_EXTENSION_POINT_JOB_CONFIG_BUILDER_ATTRIBUTE,
+                    SimulizarConstants.MODEL_LOAD_EXTENSION_POINT_JOB_ATTRIBUTE, loadJob.getClass().getName());
+            if (builder != null) {
+                // may be null as it is an optional attribute
+                loadJob.setJobConfiguration(builder.buildConfiguration(configuration.getAttributes()));
+            }
+            this.add(loadJob);
+        }
+    }
 }
