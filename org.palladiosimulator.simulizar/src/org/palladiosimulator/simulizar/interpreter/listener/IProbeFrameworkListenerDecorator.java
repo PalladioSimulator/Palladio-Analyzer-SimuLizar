f7df54bb962e3de0529295e3a06d0448bafb8062
piskachev <goran.piskachev@gmail.com>
2015-05-11 13:33:09 +0000

    adding metricspec for lafore
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches/Lafore@26490 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/interpreter/listener/IProbeFrameworkListenerDecorator.java
+++
@@ -1,8 +0,0 @@
-package org.palladiosimulator.simulizar.interpreter.listener;
-
-public interface IProbeFrameworkListenerDecorator {
-
-    public void setProbeFrameworkListener(ProbeFrameworkListener probeFrameworkListener);
-
-    public void registerMeasurements();
-}
