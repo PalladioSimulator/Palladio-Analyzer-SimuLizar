271ddd33272d08724a62eaa7cc6b73e6f712c620
igor_rogic <unknown.mail@emailaddr>
2015-05-13 13:25:55 +0000

    PALLADIOINC-58. Closed projects are handled now.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@26525 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.measuringpoint.create/src/org/palladiosimulator/simulizar/measuringpoint/create/Activator.java
+++ org.palladiosimulator.simulizar.measuringpoint.create/src/org/palladiosimulator/simulizar/measuringpoint/create/Activator.java
@@ -1,9 +1,7 @@
 package org.palladiosimulator.simulizar.measuringpoint.create;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
