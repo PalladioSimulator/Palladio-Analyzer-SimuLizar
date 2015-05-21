32168d96be27364e8b8ff4b8267121f57f469be7
piskachev <goran.piskachev@gmail.com>
2015-05-21 12:57:56 +0000

    renaming
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches/Lafore@26674 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.simulizar.lafore.mapeloop/src/org/palladiosimulator/simulizar/lafore/eurema/analyze/AnalyzeImplementation.java
@@ -0,0 +1,28 @@
+package org.palladiosimulator.simulizar.lafore.eurema.analyze;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import de.mdelab.eurema.operation.IModelOperation;
+import de.mdelab.eurema.operation.ModelOperationResult;
+
+public class AnalyzeImplementation implements IModelOperation{
+	
+	@Override
+	public ModelOperationResult run(List<Resource> models) {
+
+		System.out.println("Executing the model operations implementation: "
+				+ this.getClass().getCanonicalName());
+
+		List<Resource> output = new LinkedList<Resource>();
+		// TODO: implement the analysis here!!!
+
+		ModelOperationResult result = new ModelOperationResult(
+				"updated model", output);
+
+		return result;
+	}
+
+}
