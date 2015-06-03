026d50533de1846e916bc7bd0f95b1dbd6211dfb
ChristianStier <chrisstier@gmx.de>
2015-06-03 14:49:43 +0000

    Added Process class.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@26840 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ TransientEffects/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/reconfiguration/ReconfigurationProcess.java
@@ -0,0 +1,41 @@
+package org.palladiosimulator.simulizar.reconfiguration;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.palladiosimulator.simulizar.interpreter.listener.EventType;
+import org.palladiosimulator.simulizar.interpreter.listener.ReconfigurationEvent;
+
+import de.uka.ipd.sdq.simucomframework.SimuComSimProcess;
+import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
+
+public class ReconfigurationProcess extends SimuComSimProcess {
+
+    private final EObject monitoredElement;
+    private final IReconfigurationListener reconfigurationDispatcher;
+    private final List<IReconfigurator> reconfigurators;
+
+    protected ReconfigurationProcess(SimuComModel model, String name, List<IReconfigurator> reconfigurators,
+            EObject monitoredElement, IReconfigurationListener reconfigurationDispatcher) {
+        super(model, name);
+        this.reconfigurators = reconfigurators;
+        this.monitoredElement = monitoredElement;
+        this.reconfigurationDispatcher = reconfigurationDispatcher;
+    }
+
+    @Override
+    protected void internalLifeCycle() {
+        double startReconfigurationTime = this.getModel().getSimulationControl().getCurrentSimulationTime();
+        reconfigurationDispatcher.beginReconfigurationEvent(new ReconfigurationEvent(EventType.BEGIN,
+                startReconfigurationTime));
+        for (IReconfigurator reconfigurator : reconfigurators) {
+            if (reconfigurator.checkAndExecute(monitoredElement)) {
+                LOGGER.debug("Successfully executed reconfiguration.");
+            }
+        }
+        double endReconfigurationTime = this.getModel().getSimulationControl().getCurrentSimulationTime();
+        reconfigurationDispatcher.endReconfigurationEvent(new ReconfigurationEvent(EventType.END,
+                endReconfigurationTime));
+    }
+
+}
