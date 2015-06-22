cd68aba5e44d708e0d89c0807a88f2ab4abcbeaa
matzebecker <matthias.becker@iem.fraunhofer.de>
2015-06-22 11:13:22 +0000

    Refactored model loading, removed duplicated code, part 2
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@27451 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- ReconfigurationRuleModel/org.palladiosimulator.simulizar.reconfiguration.storydiagram/src/org/palladiosimulator/simulizar/reconfiguration/storydiagram/SDReconfigurator.java
+++ ReconfigurationRuleModel/org.palladiosimulator.simulizar.reconfiguration.storydiagram/src/org/palladiosimulator/simulizar/reconfiguration/storydiagram/SDReconfigurator.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EObject;
 import org.palladiosimulator.simulizar.access.IModelAccess;
 import org.palladiosimulator.simulizar.reconfiguration.AbstractReconfigurator;
-import org.palladiosimulator.simulizar.reconfiguration.IReconfigurator;
+import org.palladiosimulator.simulizar.reconfiguration.IReconfigurationEngine;
 import org.palladiosimulator.simulizar.reconfiguration.storydiagram.modelaccess.StoryDiagramModelAccess;
 import org.palladiosimulator.simulizar.runconfig.SimuLizarWorkflowConfiguration;
 
@@ -16,7 +16,7 @@ import org.palladiosimulator.simulizar.runconfig.SimuLizarWorkflowConfiguration;
  * @author snowball
  *
  */
-public class SDReconfigurator extends AbstractReconfigurator implements IReconfigurator {
+public class SDReconfigurator extends AbstractReconfigurator implements IReconfigurationEngine {
 
     /**
      * This class' internal LOGGER.
