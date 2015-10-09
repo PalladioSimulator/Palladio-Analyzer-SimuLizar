162fd76fc5633e10c036392f353452aba8d5cc72
ChristianStier <chrisstier@gmx.de>
2015-10-09 14:29:35 +0000

    Power models now stored in main PCM partition
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@29353 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.power/src/org/palladiosimulator/simulizar/power/jobs/LoadPowerInfrastructureRepositoryIntoBlackboardJob.java
+++ org.palladiosimulator.simulizar.power/src/org/palladiosimulator/simulizar/power/jobs/LoadPowerInfrastructureRepositoryIntoBlackboardJob.java
@@ -2,7 +2,7 @@ package org.palladiosimulator.simulizar.power.jobs;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.URI;
-import org.palladiosimulator.simulizar.power.partitions.PowerInfrastructureRepositoryResourceSetPartition;
+import org.palladiosimulator.analyzer.workflow.jobs.LoadPCMModelsIntoBlackboardJob;
 import org.palladiosimulator.simulizar.power.runconfig.LoadPowerInfrastructureRepositoryIntoBlackboardJobConfig;
 import org.palladiosimulator.simulizar.power.runconfig.LoadPowerInfrastructureRepositoryIntoBlackboardJobConfigBuilder;
 
@@ -13,6 +13,7 @@ import de.uka.ipd.sdq.workflow.jobs.CleanupFailedException;
 import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
 import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
 
 /**
  * This class is an {@link AbstractWorkflowExtensionJob} implementation to load a 
@@ -87,10 +88,9 @@ public final class LoadPowerInfrastructureRepositoryIntoBlackboardJob extends Ab
         if (this.myBlackboard == null) {
             throw new IllegalStateException("Blackboard has been not set beforehand!");
         }
-        PowerInfrastructureRepositoryResourceSetPartition partition = new PowerInfrastructureRepositoryResourceSetPartition();
+        ResourceSetPartition partition = this.myBlackboard.getPartition(LoadPCMModelsIntoBlackboardJob.PCM_MODELS_PARTITION_ID);
         if (!this.path.equals("")) {
             partition.loadModel(URI.createURI(!this.path.startsWith("platform:") ? "file:///" + this.path : this.path));
-
         }
         this.myBlackboard.addPartition(POWER_INFRASTRUCTURE_REPOSITORY_MODEL_PARTITION_ID, partition);
         // now resolve all cross references from current resource to PCM
