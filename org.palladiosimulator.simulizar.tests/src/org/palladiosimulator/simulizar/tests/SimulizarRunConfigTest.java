1e78d0ee4f19d42b50c3641f3a4f18ac448270ad
florian-rosenthal <florian.rosenthal@kit.edu>
2015-10-23 15:59:02 +0000

    -slightly refactor test cases: use TemporaryFolder Rule provided by JUnit to create EDP2 repo -add test case for simulation run with optional SLO model
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@29585 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.tests/src/org/palladiosimulator/simulizar/tests/SimulizarRunConfigTest.java
+++ org.palladiosimulator.simulizar.tests/src/org/palladiosimulator/simulizar/tests/SimulizarRunConfigTest.java
@@ -3,17 +3,21 @@ package org.palladiosimulator.simulizar.tests;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.CommonPlugin;
 import org.eclipse.emf.common.util.URI;
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.palladiosimulator.edp2.impl.RepositoryManager;
 import org.palladiosimulator.edp2.models.Repository.Repository;
 import org.palladiosimulator.edp2.repository.local.LocalDirectoryRepositoryHelper;
@@ -30,61 +34,45 @@ public class SimulizarRunConfigTest {
     private static final String MODEL_FOLDER = "/org.palladiosimulator.simulizar.tests/testmodel";
     private static final String ALLOCATION_PATH = MODEL_FOLDER + "/server.allocation";
     private static final String USAGE_MODEL_PATH = MODEL_FOLDER + "/server.usagemodel";
-    private static final String REPO_PATH = MODEL_FOLDER + "/server.repository";
-    private static final String RESOURCE_ENV_PATH = MODEL_FOLDER + "/server.resourceenvironment";
-    private static final String SYSTEM_PATH = MODEL_FOLDER + "/server.system";
     private static final String MONITOR_REPO_PATH = MODEL_FOLDER + "/monitors/server.monitorrepository";
     private static final String RECONFIGURATION_RULES_FOLDER = MODEL_FOLDER + "/rules/";
     private static final String USAGE_EVOLUTION_MODEL_PATH = MODEL_FOLDER + "/usageevolution/server.usageevolution";
+    private static final String SLO_REPO_PATH = MODEL_FOLDER + "/slo/server.slo";
 
     private SimuLizarWorkflowConfiguration simulizarConfiguration;
     private SequentialBlackboardInteractingJob<MDSDBlackboard> simulizarJob;
 
-    private static Repository repo = null;
-    private static String repoId = null;
-    private static File repoFile = new File("testRepo");
+    private Repository repo = null;
+    private File repoFile;
 
     private static URI allocationUri;
     private static URI usageModelUri;
     private static URI monitorRepoUri;
     private static URI reconfigurationRulesUri;
     private static URI usageEvolutionModelUri;
+    private static URI sloRepoUri;
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder(
+            new File(CommonPlugin.resolve(URI.createPlatformPluginURI(MODEL_FOLDER, true)).toFileString()));
 
     @BeforeClass
     public static void setUpBeforeClass() {
-        repo = LocalDirectoryRepositoryHelper.initializeLocalDirectoryRepository(repoFile);
-        repoId = repo.getId();
-        RepositoryManager.addRepository(RepositoryManager.getCentralRepository(), repo);
-
-        allocationUri = URI.createPlatformPluginURI(ALLOCATION_PATH, true);
-        allocationUri = CommonPlugin.resolve(allocationUri);
-        usageModelUri = URI.createPlatformPluginURI(USAGE_MODEL_PATH, true);
-        usageModelUri = CommonPlugin.resolve(usageModelUri);
+        allocationUri = CommonPlugin.resolve(URI.createPlatformPluginURI(ALLOCATION_PATH, true));
+        usageModelUri = CommonPlugin.resolve(URI.createPlatformPluginURI(USAGE_MODEL_PATH, true));
+        // do not resolve URI of monitor repository location!
         monitorRepoUri = URI.createPlatformPluginURI(MONITOR_REPO_PATH, true);
-        reconfigurationRulesUri = URI.createPlatformPluginURI(RECONFIGURATION_RULES_FOLDER, true);
-        reconfigurationRulesUri = CommonPlugin.resolve(reconfigurationRulesUri);
-        usageEvolutionModelUri = URI.createPlatformPluginURI(USAGE_EVOLUTION_MODEL_PATH, true);
-        usageEvolutionModelUri = CommonPlugin.resolve(usageEvolutionModelUri);
-
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        RepositoryManager.removeRepository(RepositoryManager.getCentralRepository(), repo);
-        repo = null;
-        repoId = null;
-        if (repoFile.canRead() && repoFile.canWrite()) {
-            if (repoFile.isDirectory()) {
-                for (final File f : repoFile.listFiles()) {
-                    f.delete();
-                }
-            }
-            repoFile.delete();
-        }
+        reconfigurationRulesUri = CommonPlugin.resolve(URI.createPlatformPluginURI(RECONFIGURATION_RULES_FOLDER, true));
+        usageEvolutionModelUri = CommonPlugin.resolve(URI.createPlatformPluginURI(USAGE_EVOLUTION_MODEL_PATH, true));
+        sloRepoUri = CommonPlugin.resolve(URI.createPlatformPluginURI(SLO_REPO_PATH, true));
     }
 
     @Before
     public void setUp() throws Exception {
+        this.repoFile = this.tempFolder.newFolder("testRepo");
+        this.repo = LocalDirectoryRepositoryHelper.initializeLocalDirectoryRepository(repoFile);
+        RepositoryManager.addRepository(RepositoryManager.getCentralRepository(), this.repo);
+
         Map<String, Object> properties = createSimulationProperties();
 
         this.simulizarConfiguration = new SimuLizarWorkflowConfiguration(properties);
@@ -96,7 +84,14 @@ public class SimulizarRunConfigTest {
         this.simulizarConfiguration.setUsageEvolutionFile(SimulizarConstants.DEFAULT_USAGEEVOLUTION_FILE);
         this.simulizarConfiguration.setSimuComConfiguration(new SimuComConfig(properties, false));
 
-        this.simulizarJob = new MinimalPCMInterpreterRootCompositeJob(this.simulizarConfiguration);
+        MDSDBlackboard blackboard = new MDSDBlackboard();
+        this.simulizarJob = new MinimalPCMInterpreterRootCompositeJob(this.simulizarConfiguration, blackboard);
+
+    }
+
+    @After
+    public void teraDown() {
+        RepositoryManager.removeRepository(RepositoryManager.getCentralRepository(), this.repo);
     }
 
     @Test
@@ -104,12 +99,15 @@ public class SimulizarRunConfigTest {
         // run the simulation with no optional arguments such as SLO file, action repo,
         // reconfigurations
         // the simulation should finish properly
-        try {
-            this.simulizarJob.execute(new NullProgressMonitor());
-        } catch (Throwable anyException) {
-            failDueToException(anyException);
-        }
+        runSuccessfulSimulation();
+    }
 
+    @Test
+    public void testSuccessfulSimulationRunWithSLO() {
+        // run the simulation with just the service level objects defined
+        // the simulation should finish properly
+        this.simulizarConfiguration.setServiceLevelObjectivesFile(sloRepoUri.toFileString());
+        runSuccessfulSimulation();
     }
 
     @Test
@@ -117,23 +115,23 @@ public class SimulizarRunConfigTest {
         // run the simulation with just the reconfigurations defined
         // the simulation should finish properly
         this.simulizarConfiguration.setReconfigurationRulesFolder(reconfigurationRulesUri.toFileString());
-        try {
-            this.simulizarJob.execute(new NullProgressMonitor());
-        } catch (Throwable anyException) {
-            failDueToException(anyException);
-        }
+        runSuccessfulSimulation();
     }
 
     @Test
-    public void testSuccessfulSimulationRunWithUsageEvolutionModel() {
+    public void testSuccessfulSimulationRunWithEmptyReconfigurationFolder() throws IOException {
+        File emptyRulesFolder = this.tempFolder.newFolder();
+        this.simulizarConfiguration
+                .setReconfigurationRulesFolder(emptyRulesFolder.toPath().normalize().toAbsolutePath().toString());
+        runSuccessfulSimulation();
+    }
+
+    @Test
+    public void testSuccessfulSimulationRunWithUsageEvolution() {
         // run the simulation with just the usage evolution defined
         // the simulation should finish properly
         this.simulizarConfiguration.setUsageEvolutionFile(usageEvolutionModelUri.toFileString());
-        try {
-            this.simulizarJob.execute(new NullProgressMonitor());
-        } catch (Throwable anyException) {
-            failDueToException(anyException);
-        }
+        runSuccessfulSimulation();
     }
 
     @Test
@@ -141,21 +139,24 @@ public class SimulizarRunConfigTest {
         // run the simulation with just the monitors defined
         // the simulation should finish properly
         this.simulizarConfiguration.setMonitorRepositoryFile(monitorRepoUri.toString());
-        try {
-            this.simulizarJob.execute(new NullProgressMonitor());
-        } catch (Throwable anyException) {
-            failDueToException(anyException);
-        }
+        runSuccessfulSimulation();
     }
 
-    private static Map<String, Object> createSimulationProperties() {
+    @Test
+    public void testSuccessfulSimulationRunWithMonitorRepositoryAndReconfigurationFolder() {
+        this.simulizarConfiguration.setReconfigurationRulesFolder(reconfigurationRulesUri.toFileString());
+        this.simulizarConfiguration.setMonitorRepositoryFile(monitorRepoUri.toString());
+        runSuccessfulSimulation();
+    }
+
+    private Map<String, Object> createSimulationProperties() {
         Map<String, Object> properties = new HashMap<>();
 
         properties.put(SimuComConfig.SIMULATE_FAILURES, false);
         properties.put(SimuComConfig.SIMULATE_LINKING_RESOURCES, false);
         properties.put(SimuComConfig.USE_FIXED_SEED, false);
         properties.put(SimuComConfig.PERSISTENCE_RECORDER_NAME, "Experiment Data Persistency & Presentation (EDP2)");
-        properties.put("EDP2RepositoryID", repoId);
+        properties.put("EDP2RepositoryID", this.repo.getId());
         properties.put(SimuComConfig.SIMULATOR_ID, "de.uka.ipd.sdq.codegen.simucontroller.simulizar");
         properties.put(SimuComConfig.EXPERIMENT_RUN, SimuComConfig.DEFAULT_EXPERIMENT_RUN);
         properties.put(SimuComConfig.SIMULATION_TIME, SimuComConfig.DEFAULT_SIMULATION_TIME);
@@ -166,6 +167,15 @@ public class SimulizarRunConfigTest {
                 SimulizarConstants.DEFAULT_RECONFIGURATION_RULES_FOLDER);
 
         return properties;
+    }
+
+    private void runSuccessfulSimulation() {
+        IProgressMonitor progressMonitor = new NullProgressMonitor();
+        try {
+            this.simulizarJob.execute(progressMonitor);
+        } catch (Throwable anyException) {
+            failDueToException(anyException);
+        }
     }
 
     private static void failDueToException(Throwable unexpectedException) {
