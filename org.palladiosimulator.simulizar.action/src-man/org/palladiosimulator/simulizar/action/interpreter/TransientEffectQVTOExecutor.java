81392aee67f8f56c931afb40bff16c0bb326b619
florian-rosenthal <florian.rosenthal@kit.edu>
2015-11-27 14:41:00 +0000

    adaptations due to changes of required interface
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/trunk@30060 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.action/src-man/org/palladiosimulator/simulizar/action/interpreter/TransientEffectQVTOExecutor.java
+++ org.palladiosimulator.simulizar.action/src-man/org/palladiosimulator/simulizar/action/interpreter/TransientEffectQVTOExecutor.java
@@ -26,126 +26,124 @@ import org.palladiosimulator.simulizar.reconfiguration.qvto.util.TransformationD
 import org.palladiosimulator.simulizar.reconfiguration.qvto.util.TransformationParameterInformation;
 
 /**
- * Implementation of the {@link AbstractQVTOExecutor} suitable for executing
- * {@link AdaptationStep AdaptationSteps}, i.e., transient effects.
+ * Implementation of the {@link AbstractQVTOExecutor} suitable for executing {@link AdaptationStep
+ * AdaptationSteps}, i.e., transient effects.
  * 
  * @author Florian Rosenthal
  *
  */
 class TransientEffectQVTOExecutor extends AbstractQVTOExecutor {
 
-	private final List<ModelExtent> currentPureOutParams;
-	private final URI adaptationStepUri;
-	private final URI controllerCompletionUri;
-	private final URI preconditionUri;
+    private final List<ModelExtent> currentPureOutParams;
+    private final URI adaptationStepUri;
+    private final URI controllerCompletionUri;
+    private final URI preconditionUri;
 
-	private static final EPackage MAPPING_EPACKAGE = MappingPackage.Literals.MAPPING.getEPackage();
-	private static final EPackage REPOSITORY_EPACKAGE = RepositoryPackage.Literals.REPOSITORY.getEPackage();
-	private static final EPackage ACTION_EPACKAGE = CorePackage.Literals.ACTION.getEPackage();
-	private static final EPackage ROLE_SET_EPACKAGE = InstancePackage.Literals.ROLE_SET.getEPackage();
+    private static final EPackage MAPPING_EPACKAGE = MappingPackage.Literals.MAPPING.getEPackage();
+    private static final EPackage REPOSITORY_EPACKAGE = RepositoryPackage.Literals.REPOSITORY.getEPackage();
+    private static final EPackage ACTION_EPACKAGE = CorePackage.Literals.ACTION.getEPackage();
+    private static final EPackage ROLE_SET_EPACKAGE = InstancePackage.Literals.ROLE_SET.getEPackage();
 
-	protected TransientEffectQVTOExecutor(QVToModelCache availableModels, AdaptationStep adaptationStep) {
-		super(new TransformationCache(), Objects.requireNonNull(availableModels));
+    protected TransientEffectQVTOExecutor(QVToModelCache availableModels, AdaptationStep adaptationStep) {
+        super(new TransformationCache(), Objects.requireNonNull(availableModels));
 
-		this.adaptationStepUri = URI.createURI(Objects.requireNonNull(adaptationStep.getAdaptationStepURI()));
-		this.controllerCompletionUri = URI.createURI(adaptationStep.getControllerCompletionURI());
-		this.preconditionUri = URI.createURI(adaptationStep.getPreconditionURI());
-		this.currentPureOutParams = new ArrayList<>();
+        this.adaptationStepUri = URI.createURI(Objects.requireNonNull(adaptationStep.getAdaptationStepURI()));
+        this.controllerCompletionUri = URI.createURI(adaptationStep.getControllerCompletionURI());
+        this.preconditionUri = URI.createURI(adaptationStep.getPreconditionURI());
+        this.currentPureOutParams = new ArrayList<>();
 
-		getAvailableTransformations().store(this.adaptationStepUri, this.controllerCompletionUri, this.preconditionUri);
-		getAvailableModels().storeModel(adaptationStep);
+        getAvailableTransformations().store(this.adaptationStepUri, this.controllerCompletionUri, this.preconditionUri);
+        getAvailableModels().storeModel(adaptationStep);
 
-		validateControllerCompletion();
-		validateAdaptationStep();
-		validatePrecondition();
-	}
+        validateControllerCompletion();
+        validateAdaptationStep();
+        validatePrecondition();
+    }
 
-	private void validatePrecondition() {
-		TransformationData preconditionData = getAvailableTransformations().get(this.preconditionUri);
+    private void validatePrecondition() {
+        TransformationData preconditionData = getAvailableTransformations().get(this.preconditionUri).get();
 
-		Collection<TransformationParameterInformation> inParams = preconditionData.getInParameters();
-		if (inParams.parallelStream().noneMatch(paramTypePredicate(ACTION_EPACKAGE))
-				&& inParams.parallelStream().noneMatch(paramTypePredicate(ROLE_SET_EPACKAGE))) {
-			throw new RuntimeException(
-					"Precondition " + this.preconditionUri + " must have at least 'in'/'inout' parameters of type "
-							+ ACTION_EPACKAGE.getNsURI() + " and " + ROLE_SET_EPACKAGE.getNsURI());
-		}
-	}
+        Collection<TransformationParameterInformation> inParams = preconditionData.getInParameters();
+        if (inParams.parallelStream().noneMatch(paramTypePredicate(ACTION_EPACKAGE))
+                && inParams.parallelStream().noneMatch(paramTypePredicate(ROLE_SET_EPACKAGE))) {
+            throw new RuntimeException(
+                    "Precondition " + this.preconditionUri + " must have at least 'in'/'inout' parameters of type "
+                            + ACTION_EPACKAGE.getNsURI() + " and " + ROLE_SET_EPACKAGE.getNsURI());
+        }
+    }
 
-	private void validateControllerCompletion() {
-		TransformationData controllerCompletionData = getAvailableTransformations().get(this.controllerCompletionUri);
+    private void validateControllerCompletion() {
+        TransformationData controllerCompletionData = getAvailableTransformations().get(this.controllerCompletionUri)
+                .get();
 
-		Collection<TransformationParameterInformation> outParams = controllerCompletionData.getPureOutParameters();
-		if (outParams.size() != 1 || !outParams.iterator().next().getParameterType().equals(MAPPING_EPACKAGE)) {
-			throw new RuntimeException("Controller completion " + this.controllerCompletionUri
-					+ " must have exactly one 'out' parameter of type " + MAPPING_EPACKAGE.getNsURI());
-		}
-		Collection<TransformationParameterInformation> inParams = controllerCompletionData.getInParameters();
-		if (inParams.parallelStream().noneMatch(paramTypePredicate(REPOSITORY_EPACKAGE))
-				&& inParams.parallelStream().noneMatch(paramTypePredicate(ACTION_EPACKAGE))
-				&& inParams.parallelStream().noneMatch(paramTypePredicate(ROLE_SET_EPACKAGE))) {
-			throw new RuntimeException("Controller completion " + this.controllerCompletionUri
-					+ " must have at least 'in'/'inout' parameters of type " + REPOSITORY_EPACKAGE.getNsURI() + ", "
-					+ ACTION_EPACKAGE.getNsURI() + " and " + ROLE_SET_EPACKAGE.getNsURI());
-		}
-	}
+        Collection<TransformationParameterInformation> outParams = controllerCompletionData.getPureOutParameters();
+        if (outParams.size() != 1 || !outParams.iterator().next().getParameterType().equals(MAPPING_EPACKAGE)) {
+            throw new RuntimeException("Controller completion " + this.controllerCompletionUri
+                    + " must have exactly one 'out' parameter of type " + MAPPING_EPACKAGE.getNsURI());
+        }
+        Collection<TransformationParameterInformation> inParams = controllerCompletionData.getInParameters();
+        if (inParams.parallelStream().noneMatch(paramTypePredicate(REPOSITORY_EPACKAGE))
+                && inParams.parallelStream().noneMatch(paramTypePredicate(ACTION_EPACKAGE))
+                && inParams.parallelStream().noneMatch(paramTypePredicate(ROLE_SET_EPACKAGE))) {
+            throw new RuntimeException("Controller completion " + this.controllerCompletionUri
+                    + " must have at least 'in'/'inout' parameters of type " + REPOSITORY_EPACKAGE.getNsURI() + ", "
+                    + ACTION_EPACKAGE.getNsURI() + " and " + ROLE_SET_EPACKAGE.getNsURI());
+        }
+    }
 
-	private void validateAdaptationStep() {
-		TransformationData adaptationStepData = getAvailableTransformations().get(this.adaptationStepUri);
+    private void validateAdaptationStep() {
+        TransformationData adaptationStepData = getAvailableTransformations().get(this.adaptationStepUri).get();
 
-		Collection<TransformationParameterInformation> inParams = adaptationStepData.getInParameters();
-		if (inParams.parallelStream().noneMatch(paramTypePredicate(MAPPING_EPACKAGE))
-				&& inParams.parallelStream().noneMatch(paramTypePredicate(ROLE_SET_EPACKAGE))) {
-			throw new RuntimeException(
-					"AdaptationStep " + this.adaptationStepUri + " must have at least 'in'/'inout' parameters of type "
-							+ MAPPING_EPACKAGE.getNsURI() + " and " + ROLE_SET_EPACKAGE.getNsURI());
-		}
-	}
+        Collection<TransformationParameterInformation> inParams = adaptationStepData.getInParameters();
+        if (inParams.parallelStream().noneMatch(paramTypePredicate(MAPPING_EPACKAGE))
+                && inParams.parallelStream().noneMatch(paramTypePredicate(ROLE_SET_EPACKAGE))) {
+            throw new RuntimeException(
+                    "AdaptationStep " + this.adaptationStepUri + " must have at least 'in'/'inout' parameters of type "
+                            + MAPPING_EPACKAGE.getNsURI() + " and " + ROLE_SET_EPACKAGE.getNsURI());
+        }
+    }
 
-	private static Predicate<? super TransformationParameterInformation> paramTypePredicate(EPackage desiredType) {
-		return param -> param.getParameterType().equals(desiredType);
-	}
+    private static Predicate<? super TransformationParameterInformation> paramTypePredicate(EPackage desiredType) {
+        return param -> param.getParameterType().equals(desiredType);
+    }
 
-	final boolean executePrecondition() {
-		return executeTransformation(this.preconditionUri);
-	}
+    final boolean executePrecondition() {
+        return executeTransformation(this.preconditionUri);
+    }
 
-	final boolean executeAdaptationStep() {
-		return executeTransformation(this.adaptationStepUri);
-	}
+    final boolean executeAdaptationStep() {
+        return executeTransformation(this.adaptationStepUri);
+    }
 
-	final Optional<Mapping> executeControllerCompletion(Repository controllerCompletionRepository) {
-		// cache repo, if present
-		Optional<EObject> cachedRepo = Optional.ofNullable(getAvailableModels().getModelByType(REPOSITORY_EPACKAGE));
-		getAvailableModels().storeModel(controllerCompletionRepository);
-		boolean result = executeTransformation(this.controllerCompletionUri);
-		// restore if necessary
-		cachedRepo.ifPresent(repoInCache -> getAvailableModels().storeModel(repoInCache));
-		if (result) {
-			EObject resultObj = getAvailableModels().getModelByType(MAPPING_EPACKAGE);
-			if (resultObj != null) {
-				return Optional.of((Mapping) resultObj);
-			}
-		}
-		return Optional.empty();
-	}
+    final Optional<Mapping> executeControllerCompletion(Repository controllerCompletionRepository) {
+        // cache repo, if present
+        Optional<EObject> cachedRepo = getAvailableModels().getModelByType(REPOSITORY_EPACKAGE);
+        getAvailableModels().storeModel(controllerCompletionRepository);
+        boolean result = executeTransformation(this.controllerCompletionUri);
+        // restore if necessary
+        cachedRepo.ifPresent(repoInCache -> getAvailableModels().storeModel(repoInCache));
+        if (result) {
+            return getAvailableModels().getModelByType(MAPPING_EPACKAGE).map(obj -> (Mapping) obj);
+        }
+        return Optional.empty();
+    }
 
-	@Override
-	protected boolean handleExecutionResult(ExecutionDiagnostic executionResult) {
-		boolean result = super.handleExecutionResult(executionResult);
-		if (result) {
-			this.currentPureOutParams.stream().map(ModelExtent::getContents).filter(contents -> !contents.isEmpty())
-					.forEach(contents -> getAvailableModels().storeModel(contents.get(0)));
-		}
-		return result;
-	}
+    @Override
+    protected boolean handleExecutionResult(ExecutionDiagnostic executionResult) {
+        boolean result = super.handleExecutionResult(executionResult);
+        if (result) {
+            this.currentPureOutParams.stream().map(ModelExtent::getContents).filter(contents -> !contents.isEmpty())
+                    .forEach(contents -> getAvailableModels().storeModel(contents.get(0)));
+        }
+        return result;
+    }
 
-	@Override
-	protected ModelExtent[] setupModelExtents(TransformationData data) {
-		this.currentPureOutParams.clear();
-		ModelExtent[] result = super.setupModelExtents(data);
-		data.getPureOutParameters()
-				.forEach(outParam -> this.currentPureOutParams.add(result[outParam.getParameterIndex()]));
-		return result;
-	}
+    @Override
+    protected ModelExtent[] setupModelExtents(TransformationData data) {
+        this.currentPureOutParams.clear();
+        ModelExtent[] result = super.setupModelExtents(data);
+        data.getPureOutParameters()
+                .forEach(outParam -> this.currentPureOutParams.add(result[outParam.getParameterIndex()]));
+        return result;
+    }
 }
