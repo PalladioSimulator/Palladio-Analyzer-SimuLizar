e62068402dfe0b8e4d441d41418b88b806d39910
ChristianStier <chrisstier@gmx.de>
2016-01-22 08:51:50 +0000

    Added branch with minor changes to aggregation functions for the case study.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@30598 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ TransientEffectsCaseStudy/org.palladiosimulator.simulizar.action.repository/src/org/palladiosimulator/simulizar/action/repository/Activator.java
@@ -0,0 +1,25 @@
+package org.palladiosimulator.simulizar.action.repository;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		Activator.context = context;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		Activator.context = null;
+	}
+
+}
