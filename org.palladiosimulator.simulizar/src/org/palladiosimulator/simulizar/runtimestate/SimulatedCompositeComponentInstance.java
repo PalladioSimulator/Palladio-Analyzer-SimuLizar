4fbc47124b7897d8bece8767cdbfb4de91106c42
igor_rogic <unknown.mail@emailaddr>
2016-02-19 09:39:21 +0000

    Remove the SimuLizar plugin.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches/Elasticity%20and%20RR%20model%20merge@31093 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/runtimestate/SimulatedCompositeComponentInstance.java
+++
@@ -1,9 +0,0 @@
-package org.palladiosimulator.simulizar.runtimestate;
-
-public class SimulatedCompositeComponentInstance extends SimulatedComponentInstance {
-
-    public SimulatedCompositeComponentInstance(final SimuLizarRuntimeStateAbstract runtimeState, final FQComponentID fqId) {
-        super(runtimeState, fqId);
-    }
-
-}
