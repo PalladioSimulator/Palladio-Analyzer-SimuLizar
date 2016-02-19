2676f663596a9d245cd643ea5305c19cda72b08f
igor_rogic <unknown.mail@emailaddr>
2016-02-19 08:44:42 +0000

    Moving the rest of the Elasticity analysis branch to the one for merge.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@31088 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Elasticity and RR model merge/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/exceptions/SLORepositoryLoadException.java
@@ -0,0 +1,18 @@
+package org.palladiosimulator.simulizar.exceptions;
+
+public class SLORepositoryLoadException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3086193563598634902L;
+
+    /**
+     * @param message
+     *            the exception message.
+     */
+    public SLORepositoryLoadException(final String message) {
+        super(message);
+    }
+
+}
---
+++ Elasticity and RR model merge/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/reconfiguration/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package containing the interfaces and listeners needed to trigger and implement reconfigurations
+ * on the model@runtime.
+ *
+ * @author Steffen Becker
+ */
+package org.palladiosimulator.simulizar.reconfiguration;
