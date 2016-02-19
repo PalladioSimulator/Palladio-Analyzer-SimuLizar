5ffc6c0014d32b764c0fc611d04fbc798fadefad
igor_rogic <unknown.mail@emailaddr>
2016-02-19 08:42:46 +0000

    Moving elasticity plugin to the branch for merging
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches/Elasticity%20and%20RR%20model%20merge@31086 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- DynamicWorkload/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/reconfiguration/qvto/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Package which implements an reconfigurator based on QVTo transformations.
- * 
- * @author Matthias Becker
- *
- */
-package org.palladiosimulator.simulizar.reconfiguration.qvto;
