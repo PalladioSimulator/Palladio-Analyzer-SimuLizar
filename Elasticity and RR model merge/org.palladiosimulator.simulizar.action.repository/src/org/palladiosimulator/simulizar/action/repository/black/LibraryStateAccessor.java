ed0f27aa1e9189b07c05a9d8bbb319981a1ae2a9
igor_rogic <unknown.mail@emailaddr>
2016-03-24 09:57:54 +0000

    New actions repository.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@31513 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ Elasticity and RR model merge/org.palladiosimulator.simulizar.action.repository/src/org/palladiosimulator/simulizar/action/repository/black/LibraryStateAccessor.java
@@ -0,0 +1,16 @@
+package org.palladiosimulator.simulizar.action.repository.black;
+
+import org.palladiosimulator.simulizar.runtimestate.IRuntimeStateAccessor;
+import org.palladiosimulator.simulizar.runtimestate.SimuLizarRuntimeState;
+import org.palladiosimulator.simulizar.runtimestate.SimuLizarRuntimeStateAbstract;
+
+public final class LibraryStateAccessor implements IRuntimeStateAccessor {
+
+    public LibraryStateAccessor() {
+    }
+
+    @Override
+    public void setRuntimeStateModel(SimuLizarRuntimeStateAbstract state) {
+        SimulationStateLibrary.injectRuntimeStateModel(state);
+    }
+}
