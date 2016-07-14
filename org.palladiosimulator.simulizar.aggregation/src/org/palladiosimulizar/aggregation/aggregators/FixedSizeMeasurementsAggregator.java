05dd2dba9c49d2b58ba64849c47eca3d4b18e023
florian-rosenthal <florian.rosenthal@kit.edu>
2016-07-14 14:56:49 +0000

    first draft of fixed size measurements aggregation is now complete
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches/extendedMonitorRepository@32494 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.simulizar.aggregation/src/org/palladiosimulizar/aggregation/aggregators/FixedSizeMeasurementsAggregator.java
+++ org.palladiosimulator.simulizar.aggregation/src/org/palladiosimulizar/aggregation/aggregators/FixedSizeMeasurementsAggregator.java
@@ -1,32 +1,54 @@
 package org.palladiosimulizar.aggregation.aggregators;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Objects;
 import java.util.Optional;
 
+import javax.measure.Measure;
+import javax.measure.quantity.Duration;
+
+import org.apache.commons.collections15.IteratorUtils;
+import org.jscience.physics.amount.Amount;
 import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
 import org.palladiosimulator.edp2.util.MetricDescriptionUtility;
+import org.palladiosimulator.experimentanalysis.statisticalcharacterization.aggregators.StatisticalCharacterizationAggregator;
 import org.palladiosimulator.measurementframework.MeasuringValue;
 import org.palladiosimulator.measurementframework.listener.IMeasurementSourceListener;
+import org.palladiosimulator.measurementframework.listener.MeasurementSource;
 import org.palladiosimulator.metricspec.BaseMetricDescription;
 import org.palladiosimulator.metricspec.MetricDescription;
+import org.palladiosimulator.metricspec.NumericalBaseMetricDescription;
+import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.monitorrepository.FixedSizeAggregation;
 import org.palladiosimulator.monitorrepository.MeasurementSpecification;
 import org.palladiosimulator.probeframework.calculator.Calculator;
 import org.palladiosimulator.probeframework.calculator.RegisterCalculatorFactoryDecorator;
 
-public class FixedSizeMeasurementsAggregator {
+import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
+
+public class FixedSizeMeasurementsAggregator extends MeasurementSource {
 
     private final MeasurementSpecification measurementSpecification;
+    private final MetricDescription expectedMetric;
     private final FixedSizeAggregation fixedSizeAggregation;
+    private final StatisticalCharacterizationAggregator aggregator;
+    private final InternalBuffer buffer;
+
+    private final SimuComModel simuComModel;
+
+    private int measurementsUntilNextAggregation = 0;
 
     public FixedSizeMeasurementsAggregator(MeasurementSpecification measurementSpec,
-            RegisterCalculatorFactoryDecorator calcFactory) {
-        this.measurementSpecification = Objects.requireNonNull(measurementSpec);
+            RegisterCalculatorFactoryDecorator calcFactory, SimuComModel simuComModel) {
+        super(Objects.requireNonNull(measurementSpec).getMetricDescription());
+
+        this.simuComModel = Objects.requireNonNull(simuComModel);
+        this.measurementSpecification = measurementSpec;
 
         this.fixedSizeAggregation = (FixedSizeAggregation) this.measurementSpecification.getProcessingType();
-
         MeasuringPoint mp = measurementSpec.getMonitor().getMeasuringPoint();
-        MetricDescription expectedMetric = measurementSpec.getMetricDescription();
+        this.expectedMetric = measurementSpec.getMetricDescription();
         Optional<Calculator> calculator;
         BaseMetricDescription[] subsumedBaseMetrics = MetricDescriptionUtility.toBaseMetricDescriptions(expectedMetric);
         if (subsumedBaseMetrics.length == 1) {
@@ -47,19 +69,146 @@ public class FixedSizeMeasurementsAggregator {
                     + measurementSpec.getMonitor().getEntityName() + "'\n"
                     + "Ensure that measurement calculator has been created and registered within the ProbeFrameworkListener class!");
         }
+        this.aggregator = this.fixedSizeAggregation.getStatisticalCharacterization()
+                .getAggregator((NumericalBaseMetricDescription) this.expectedMetric);
+
+        this.buffer = new InternalBuffer(this.fixedSizeAggregation.getNumberOfMeasurements());
+
+        resetCounter();
         calculator.get().addObserver(new IMeasurementSourceListener() {
 
             @Override
             public void preUnregister() {
-                // TODO Auto-generated method stub
+                FixedSizeMeasurementsAggregator.this.buffer.clear();
 
             }
 
             @Override
             public void newMeasurementAvailable(MeasuringValue newMeasurement) {
-                // TODO Auto-generated method stub
-
+                FixedSizeMeasurementsAggregator.this.collectMeasurement(newMeasurement);
             }
         });
+    }
+
+    private void collectMeasurement(MeasuringValue newMeasurement) {
+        if (!Objects.requireNonNull(newMeasurement).isCompatibleWith(this.expectedMetric)
+                && !MetricDescriptionUtility.isBaseMetricDescriptionSubsumedByMetricDescription(
+                        (BaseMetricDescription) this.expectedMetric, newMeasurement.getMetricDesciption())) {
+            throw new IllegalStateException("Somehow a wrong measurement kind was passed.");
+        }
+        this.buffer.add(newMeasurement);
+
+        decrementCounter();
+        if (aggregationRequired()) {
+            aggregate();
+            resetCounter();
+        }
+    }
+
+    private void resetCounter() {
+        this.measurementsUntilNextAggregation = this.fixedSizeAggregation.getFrequency();
+    }
+
+    private void decrementCounter() {
+        --this.measurementsUntilNextAggregation;
+    }
+
+    private boolean aggregationRequired() {
+        return this.measurementsUntilNextAggregation == 0;
+    }
+
+    private void aggregate() {
+        Measure<Double, Duration> start = this.buffer.getOldestElement()
+                .getMeasureForMetric(MetricDescriptionConstants.POINT_IN_TIME_METRIC);
+        Measure<Double, Duration> end = this.buffer.getNewestElement()
+                .getMeasureForMetric(MetricDescriptionConstants.POINT_IN_TIME_METRIC);
+
+        MeasuringValue aggregatedData = this.aggregator.aggregateData(this.buffer,
+                Amount.valueOf(start.getValue(), start.getUnit()), Amount.valueOf(end.getValue(), end.getUnit()),
+                Optional.empty());
+
+        super.notifyMeasurementSourceListener(aggregatedData);
+    }
+
+    private static final class InternalBuffer implements Iterable<MeasuringValue> {
+
+        // the data container will be used in a circular way
+        private final MeasuringValue[] data;
+        // pointer to the least recent element of the data
+        // that is the element which was added least recently
+        // this is as base "address" for all data accesses
+        private int oldestElementPointer = 0;
+        private int currentElementCount = 0;
+
+        private InternalBuffer(int capacity) {
+            assert capacity > 0;
+            this.data = new MeasuringValue[capacity];
+        }
+
+        public int size() {
+            return this.currentElementCount;
+        }
+
+        public int capacity() {
+            return this.data.length;
+        }
+
+        public boolean isEmpty() {
+            return this.currentElementCount == 0;
+        }
+
+        public boolean isFull() {
+            return size() == capacity();
+        }
+
+        public boolean add(MeasuringValue measuringValue) {
+            if (isFull()) {
+                addFull(measuringValue);
+            } else {
+                addNotFull(measuringValue);
+            }
+            return true;
+        }
+
+        public MeasuringValue getOldestElement() {
+            return this.data[oldestElementPointer];
+        }
+
+        private void addFull(MeasuringValue newElemement) {
+            assert isFull() && newElemement != null;
+
+            this.data[this.oldestElementPointer] = newElemement;
+            adjustOldestElementPointer();
+        }
+
+        private void addNotFull(MeasuringValue elementToAdd) {
+            assert !isFull() && elementToAdd != null;
+
+            int index = (this.oldestElementPointer + this.currentElementCount) % capacity();
+            this.data[index] = elementToAdd;
+            this.currentElementCount++;
+        }
+
+        private void adjustOldestElementPointer() {
+            this.oldestElementPointer++;
+            this.oldestElementPointer %= capacity();
+            assert this.oldestElementPointer >= 0 && this.oldestElementPointer < capacity();
+        }
+
+        public void clear() {
+            Arrays.fill(data, null);
+            this.oldestElementPointer = this.currentElementCount = 0;
+
+        }
+
+        public MeasuringValue getNewestElement() {
+            return this.data[(this.oldestElementPointer + size() - 1) % capacity()];
+        }
+
+        @Override
+        public Iterator<MeasuringValue> iterator() {
+            return IteratorUtils.arrayIterator(this.data);
+        }
+
     }
 }
--- org.palladiosimulator.simulizar.aggregation/src/org/palladiosimulizar/aggregation/probeframework/AggregatorsProbeFrameworkListenerDecorator.java
+++ org.palladiosimulator.simulizar.aggregation/src/org/palladiosimulizar/aggregation/probeframework/AggregatorsProbeFrameworkListenerDecorator.java
@@ -3,14 +3,35 @@ package org.palladiosimulizar.aggregation.probeframework;
 import java.util.Collection;
 
 import org.eclipse.emf.ecore.EClass;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
+import org.palladiosimulator.metricspec.NumericalBaseMetricDescription;
 import org.palladiosimulator.monitorrepository.MeasurementSpecification;
 import org.palladiosimulator.monitorrepository.MonitorRepositoryPackage;
+import org.palladiosimulator.probeframework.calculator.RegisterCalculatorFactoryDecorator;
+import org.palladiosimulator.runtimemeasurement.RuntimeMeasurementModel;
+import org.palladiosimulator.simulizar.interpreter.listener.AbstractProbeFrameworkListener;
 import org.palladiosimulator.simulizar.interpreter.listener.AbstractRecordingProbeFrameworkListenerDecorator;
+import org.palladiosimulizar.aggregation.aggregators.FixedSizeMeasurementsAggregator;
+
+import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
 
 public class AggregatorsProbeFrameworkListenerDecorator extends AbstractRecordingProbeFrameworkListenerDecorator {
 
     private static final EClass FIXED_SIZE_AGGREGATION_ECLASS = MonitorRepositoryPackage.Literals.FIXED_SIZE_AGGREGATION;
     private static final EClass VARIABLE_SIZE_AGGREGATION_ECLASS = MonitorRepositoryPackage.Literals.VARIABLE_SIZE_AGGREGATION;
+
+    private RegisterCalculatorFactoryDecorator calculatorFactory;
+    private RuntimeMeasurementModel runtimeMeasurementModel;
+    private SimuComModel model;
+
+    @Override
+    public void setProbeFrameworkListener(AbstractProbeFrameworkListener listener) {
+        super.setProbeFrameworkListener(listener);
+        this.calculatorFactory = RegisterCalculatorFactoryDecorator.class
+                .cast(getProbeFrameworkListener().getCalculatorFactory());
+        this.model = getProbeFrameworkListener().getSimuComModel();
+        this.runtimeMeasurementModel = getProbeFrameworkListener().getRuntimeMeasurementModel();
+    }
 
     @Override
     public void registerMeasurements() {
@@ -18,8 +39,19 @@ public class AggregatorsProbeFrameworkListenerDecorator extends AbstractRecordin
 
         Collection<MeasurementSpecification> fixedSizeAggregationMeasurementSpecs = this.getProbeFrameworkListener()
                 .getMeasurementSpecificationsForProcessingType(FIXED_SIZE_AGGREGATION_ECLASS);
-        Collection<MeasurementSpecification> variableSizeAggregMeasurementSpecs = this.getProbeFrameworkListener()
+        Collection<MeasurementSpecification> variableSizeAggregationMeasurementSpecs = this.getProbeFrameworkListener()
                 .getMeasurementSpecificationsForProcessingType(VARIABLE_SIZE_AGGREGATION_ECLASS);
+
+        fixedSizeAggregationMeasurementSpecs.forEach(this::initFixedSizeAggregation);
+
+    }
+
+    private void initFixedSizeAggregation(MeasurementSpecification measurementSpecification) {
+        MeasuringPoint measuringPoint = measurementSpecification.getMonitor().getMeasuringPoint();
+        NumericalBaseMetricDescription expectedMetric = (NumericalBaseMetricDescription) measurementSpecification
+                .getMetricDescription();
+        // TODO FR move parts from FixedSizemeasurementsAggregator class here
+        new FixedSizeMeasurementsAggregator(measurementSpecification, this.calculatorFactory, this.model);
 
     }
 }
