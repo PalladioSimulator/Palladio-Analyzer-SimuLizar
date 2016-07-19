49c33dc6192bb65fde367a8ea4a8ea54e87cd607
florian-rosenthal <florian.rosenthal@kit.edu>
2016-07-19 11:36:47 +0000

    finish probe framework listener decorator for fixed and variable size measurements recording
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Incubation/SimuLizar/branches@32541 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- extendedMonitorRepository/org.palladiosimulator.simulizar.aggregation/src/org/palladiosimulizar/aggregation/probeframework/AggregatorsProbeFrameworkListenerDecorator.java
+++ extendedMonitorRepository/org.palladiosimulator.simulizar.aggregation/src/org/palladiosimulizar/aggregation/probeframework/AggregatorsProbeFrameworkListenerDecorator.java
@@ -1,17 +1,25 @@
 package org.palladiosimulizar.aggregation.probeframework;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
+import org.palladiosimulator.edp2.util.MetricDescriptionUtility;
 import org.palladiosimulator.metricspec.NumericalBaseMetricDescription;
+import org.palladiosimulator.metricspec.util.MetricSpecSwitch;
+import org.palladiosimulator.monitorrepository.FixedSizeAggregation;
 import org.palladiosimulator.monitorrepository.MeasurementSpecification;
 import org.palladiosimulator.monitorrepository.MonitorRepositoryPackage;
+import org.palladiosimulator.monitorrepository.VariableSizeAggregation;
+import org.palladiosimulator.probeframework.calculator.Calculator;
 import org.palladiosimulator.probeframework.calculator.RegisterCalculatorFactoryDecorator;
 import org.palladiosimulator.runtimemeasurement.RuntimeMeasurementModel;
 import org.palladiosimulator.simulizar.interpreter.listener.AbstractProbeFrameworkListener;
 import org.palladiosimulator.simulizar.interpreter.listener.AbstractRecordingProbeFrameworkListenerDecorator;
 import org.palladiosimulizar.aggregation.aggregators.FixedSizeMeasurementsAggregator;
+import org.palladiosimulizar.aggregation.aggregators.VariableSizeMeasurementAggregator;
 
 import de.uka.ipd.sdq.simucomframework.model.SimuComModel;
 
@@ -42,16 +50,84 @@ public class AggregatorsProbeFrameworkListenerDecorator extends AbstractRecordin
         Collection<MeasurementSpecification> variableSizeAggregationMeasurementSpecs = this.getProbeFrameworkListener()
                 .getMeasurementSpecificationsForProcessingType(VARIABLE_SIZE_AGGREGATION_ECLASS);
 
-        fixedSizeAggregationMeasurementSpecs.forEach(this::initFixedSizeAggregation);
+        fixedSizeAggregationMeasurementSpecs.stream().filter(MeasurementSpecification::isTriggersSelfAdaptations)
+                .forEach(this::initFixedSizeAggregation);
+        variableSizeAggregationMeasurementSpecs.stream().filter(MeasurementSpecification::isTriggersSelfAdaptations)
+                .forEach(this::initVariableSizeAggregation);
+    }
 
+    private void initVariableSizeAggregation(MeasurementSpecification measurementSpecification) {
+        MeasuringPoint measuringPoint = measurementSpecification.getMonitor().getMeasuringPoint();
+        Optional<NumericalBaseMetricDescription> expectedMetric = GET_NUMERICAL_BASE_METRIC_SWITCH
+                .doSwitch(measurementSpecification.getMetricDescription());
+
+        // fails in case optional is empty
+        checkValidity(expectedMetric);
+
+        Calculator correspondingBaseCalculator = getBaseCalculator(expectedMetric.get(),
+                measuringPoint).<IllegalStateException> orElseThrow(() -> new IllegalStateException(
+                        "Variable size aggregation of measurements cannot be initialized.\n" + "No '"
+                                + expectedMetric.get().getName() + "' calculator available for: " + "MeasuringPoint '"
+                                + measuringPoint.getStringRepresentation() + "'.\n" + "Affected Monitor: '"
+                                + measurementSpecification.getMonitor().getEntityName() + "'\n"
+                                + "Ensure that measurement calculator has been created and registered within the ProbeFrameworkListener class!"));
+
+        correspondingBaseCalculator.addObserver(new VariableSizeMeasurementAggregator(expectedMetric.get(),
+                this.runtimeMeasurementModel, (VariableSizeAggregation) measurementSpecification.getProcessingType()));
     }
 
     private void initFixedSizeAggregation(MeasurementSpecification measurementSpecification) {
         MeasuringPoint measuringPoint = measurementSpecification.getMonitor().getMeasuringPoint();
-        NumericalBaseMetricDescription expectedMetric = (NumericalBaseMetricDescription) measurementSpecification
-                .getMetricDescription();
-        // TODO FR move parts from FixedSizemeasurementsAggregator class here
-        new FixedSizeMeasurementsAggregator(measurementSpecification, this.calculatorFactory, this.model);
+        Optional<NumericalBaseMetricDescription> expectedMetric = GET_NUMERICAL_BASE_METRIC_SWITCH
+                .doSwitch(measurementSpecification.getMetricDescription());
 
+        // fails in case optional is empty
+        checkValidity(expectedMetric);
+
+        Calculator correspondingBaseCalculator = getBaseCalculator(expectedMetric.get(),
+                measuringPoint).<IllegalStateException> orElseThrow(() -> new IllegalStateException(
+                        "Fixed size aggregation of measurements cannot be initialized.\n" + "No '"
+                                + expectedMetric.get().getName() + "' calculator available for: " + "MeasuringPoint '"
+                                + measuringPoint.getStringRepresentation() + "'.\n" + "Affected Monitor: '"
+                                + measurementSpecification.getMonitor().getEntityName() + "'\n"
+                                + "Ensure that measurement calculator has been created and registered within the ProbeFrameworkListener class!"));
+
+        correspondingBaseCalculator.addObserver(new FixedSizeMeasurementsAggregator(expectedMetric.get(),
+                this.runtimeMeasurementModel, (FixedSizeAggregation) measurementSpecification.getProcessingType()));
     }
+
+    private Optional<Calculator> getBaseCalculator(NumericalBaseMetricDescription metric,
+            MeasuringPoint measuringPoint) {
+        Calculator baseCalculator = this.calculatorFactory
+                .getCalculatorByMeasuringPointAndMetricDescription(measuringPoint, metric);
+        if (baseCalculator == null) {
+            return this.calculatorFactory.getCalculatorsForMeasuringPoint(measuringPoint)
+                    .stream().filter(calc -> MetricDescriptionUtility
+                            .isBaseMetricDescriptionSubsumedByMetricDescription(metric, calc.getMetricDesciption()))
+                    .findAny();
+
+        }
+        return Optional.of(baseCalculator);
+    }
+
+    private static void checkValidity(Optional<NumericalBaseMetricDescription> expectedMetric) {
+        if (!expectedMetric.isPresent()) {
+            throw new IllegalStateException("So far, only " + NumericalBaseMetricDescription.class.getSimpleName()
+                    + "s are supported for fixed and variable size aggregation!");
+        }
+    }
+
+    private static final MetricSpecSwitch<Optional<NumericalBaseMetricDescription>> GET_NUMERICAL_BASE_METRIC_SWITCH = new MetricSpecSwitch<Optional<NumericalBaseMetricDescription>>() {
+
+        @Override
+        public Optional<NumericalBaseMetricDescription> caseNumericalBaseMetricDescription(
+                NumericalBaseMetricDescription numericalBaseMetricDescription) {
+            return Optional.of(numericalBaseMetricDescription);
+        }
+
+        @Override
+        public Optional<NumericalBaseMetricDescription> defaultCase(EObject eObject) {
+            return Optional.empty();
+        }
+    };
 }
