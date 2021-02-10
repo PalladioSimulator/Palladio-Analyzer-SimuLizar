2b6b0e7654269c51719d24add44bc3b92ee4d5d6
Sebastian Krach <krach@fzi.de>
2021-02-10 17:55:54 +0100

    [SIMULIZAR-153] Fix wrongful extension registration
    * removed unnecessary code
    * added partition contribution
    * activated dagger

--- bundles/org.palladiosimulator.simulizar.action/src-man/org/palladiosimulator/simulizar/action/di/ActionRuntimeComponent.java
+++ bundles/org.palladiosimulator.simulizar.action/src-man/org/palladiosimulator/simulizar/action/di/ActionRuntimeComponent.java
@@ -1,14 +1,30 @@
 package org.palladiosimulator.simulizar.action.di;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IExecutableExtensionFactory;
 import org.palladiosimulator.simulizar.action.interpreter.util.TransientEffectTransformationCacheKeeper;
 import org.palladiosimulator.simulizar.di.component.core.SimuLizarRuntimeComponent;
 import org.palladiosimulator.simulizar.di.extension.ExtensionComponent;
+import org.palladiosimulator.simulizar.scopes.RuntimeExtensionScope;
 
 import dagger.Component;
 
 @Component(dependencies = SimuLizarRuntimeComponent.class)
+@RuntimeExtensionScope
 public interface ActionRuntimeComponent extends ExtensionComponent {
     
     TransientEffectTransformationCacheKeeper cacheKeeper();
+    
+    @Component.Factory
+    public static interface Factory extends ExtensionComponent.Factory {
+        ActionRuntimeComponent create(SimuLizarRuntimeComponent runtimeComponent);
+    }
+
+    public static class EclipseFactory implements IExecutableExtensionFactory {
+        @Override
+        public Object create() throws CoreException {
+            return DaggerActionRuntimeComponent.factory();
+        }
+    }
 
 }
---
+++ bundles/org.palladiosimulator.simulizar.action/src-man/org/palladiosimulator/simulizar/action/jobs/AdaptationBehaviorPartitionContribution.java
@@ -0,0 +1,23 @@
+package org.palladiosimulator.simulizar.action.jobs;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+
+import org.palladiosimulator.simulizar.action.partitions.AdaptationBehaviorRepositoryResourceSetPartition;
+import org.palladiosimulator.simulizar.launcher.jobs.PartitionContribution;
+
+public class AdaptationBehaviorPartitionContribution implements PartitionContribution {
+    private final Provider<AdaptationBehaviorRepositoryResourceSetPartition> partitionSupplier;
+
+    @Inject
+    public AdaptationBehaviorPartitionContribution(Provider<AdaptationBehaviorRepositoryResourceSetPartition> partitionSupplier) {
+        this.partitionSupplier = partitionSupplier;
+    }
+
+    @Override
+    public void contribute(Facade delegate) {
+        delegate.appendPartition(AdaptationBehaviorModelContribution.ADAPTATION_BEHAVIOR__REPOSITORY_MODEL_PARTITION_ID,
+                partitionSupplier.get());
+    }
+
+}
