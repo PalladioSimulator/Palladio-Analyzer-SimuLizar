92849decfe515fc602370433e007ca40d927d730
Sebastian Krach <krach@fzi.de>
2022-01-13 17:06:53 +0100

    [SIMULIZAR-166] Support for deactivation of reliability simulation

--- bundles/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/di/modules/scoped/thread/CoreSimulatedThreadBindings.java
+++ bundles/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/di/modules/scoped/thread/CoreSimulatedThreadBindings.java
@@ -7,6 +7,8 @@ import org.palladiosimulator.simulizar.interpreter.RDSeffPerformanceSwitch;
 import org.palladiosimulator.simulizar.interpreter.RDSeffSwitch;
 import org.palladiosimulator.simulizar.interpreter.RDSeffSwitchContributionFactory;
 import org.palladiosimulator.simulizar.interpreter.impl.ExtensibleComposedRDSeffSwitchFactory;
+import org.palladiosimulator.simulizar.interpreter.legacy.NOPReliabilityInterpreter;
+import org.palladiosimulator.simulizar.runconfig.SimuLizarWorkflowConfiguration;
 import org.palladiosimulator.simulizar.scopes.SimulatedThreadScope;
 
 import com.google.common.collect.ImmutableSet;
@@ -28,4 +30,18 @@ public interface CoreSimulatedThreadBindings {
             RDSeffPerformanceSwitch.Factory performanceSwitchFactory, RDSeffSwitch.Factory rdseffSwitchFactory) {
         return ImmutableSet.of(rdseffSwitchFactory, performanceSwitchFactory);
     }
+    
+    @Provides
+    @ElementsIntoSet
+    static Set<RDSeffSwitchContributionFactory> provideNOPSwitches(
+            NOPReliabilityInterpreter.Factory factory, SimuLizarWorkflowConfiguration config) {
+        // The entire binding is only required to provide support to "deactivate" simulation of reliability elements in models which contain them.
+        if (config.getSimulateFailures()) {
+            //The reliability interpreter is provided by an extension not by the core
+            return ImmutableSet.of();
+        } else {
+            return ImmutableSet.of(factory);
+        }
+    }
+
 }
