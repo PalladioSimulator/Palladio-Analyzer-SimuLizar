6825b0974501fcfff97aae7c2970f51fa1100955
nathan <nathan@familie-hagel.de>
2022-09-02 02:06:46 +0200

    Implement test to test Action execution without resource demand.

--- tests/org.palladiosimulator.simulizar.tests/src/org/palladiosimulator/simulizar/simulation/tests/ActionWithoutResourceDemandingStepTest.java
+++ tests/org.palladiosimulator.simulizar.tests/src/org/palladiosimulator/simulizar/simulation/tests/ActionWithoutResourceDemandingStepTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.palladiosimulator.edp2.models.ExperimentData.ExperimentRun;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.pcm.usagemodel.UsageScenario;
+import org.palladiosimulator.simulizar.action.di.DaggerActionRuntimeComponent;
 import org.palladiosimulator.simulizar.launcher.SimulizarConstants;
 import org.palladiosimulator.simulizar.reconfiguration.qvto.DaggerQVTOReconfigurationComponent;
 import org.palladiosimulator.simulizar.test.commons.annotation.LoadPCMInstanceFromBundle;
@@ -29,50 +30,47 @@ import tools.mdsd.junit5utils.annotations.PluginTestOnly;
 
 @PluginTestOnly
 public class ActionWithoutResourceDemandingStepTest {
-	
+
 	@Test
-    @LoadPCMInstanceFromBundle(bundleName = "org.palladiosimulator.simulizar.tests", basePath = "testmodels/actionTestWithoutResourceDemand", modelFiles = {
-            "default.allocation", "default.usagemodel", "default.repository", "defaultMonitor.monitorrepository" })
-    @UseSimuLizarExtension(DaggerQVTOReconfigurationComponent.class)
-    @SimulationConfig(maxMeasurements = "100")
-    @SetConfigProperty(id = SimulizarConstants.RECONFIGURATION_RULES_FOLDER, value = "platform:/plugin/org.palladiosimulator.simulizar.tests/testmodels/actionTestWithoutResourceDemand/reconfigurations")
-    @RunSimuLizar
-    void testPopulationScaleAction(UsageScenario scenario, ExperimentRun expRun)
-            throws JobFailedException, UserCanceledException {
+	@LoadPCMInstanceFromBundle(bundleName = "org.palladiosimulator.simulizar.tests", basePath = "testmodels/actionTestWithoutResourceDemand", modelFiles = {
+			"default.allocation", "default.usagemodel", "default.repository", "defaultMonitor.monitorrepository",
+			"org.palladiosimulator.simulizar.action.repository/model/repository.actions" })
+	@UseSimuLizarExtension(DaggerQVTOReconfigurationComponent.class)
+	@SimulationConfig(maxMeasurements = "100")
+	@SetConfigProperty(id = SimulizarConstants.RECONFIGURATION_RULES_FOLDER, value = "platform:/plugin/org.palladiosimulator.simulizar.tests/testmodels/actionTestWithoutResourceDemand/reconfigurations")
+	@RunSimuLizar
+	@UseSimuLizarExtension(DaggerActionRuntimeComponent.class)
+	void testPopulationScaleAction(UsageScenario scenario, ExperimentRun expRun)
+			throws JobFailedException, UserCanceledException {
 		var measurement = MeasurementTestUtils.getMeasurementOfAt(expRun.getMeasurement(),
-                MetricDescriptionConstants.RESPONSE_TIME_METRIC_TUPLE, scenario);
-        assertTrue(measurement.isPresent());
+				MetricDescriptionConstants.RESPONSE_TIME_METRIC_TUPLE, scenario);
+		assertTrue(measurement.isPresent());
 
-        List<Measure<?, Duration>> responseTimeMeasurements = MeasurementTestUtils
-            .allMeasurementsOfMetric(measurement.get(), MetricDescriptionConstants.RESPONSE_TIME_METRIC);
-        List<Measure<?, Duration>> timeMeasurements = MeasurementTestUtils.allMeasurementsOfMetric(measurement.get(),
-                MetricDescriptionConstants.POINT_IN_TIME_METRIC);
-        assertEquals(responseTimeMeasurements.size(), timeMeasurements.size());
-		
-        double expectedMeasurementTime = 1.0;
+		List<Measure<?, Duration>> responseTimeMeasurements = MeasurementTestUtils
+				.allMeasurementsOfMetric(measurement.get(), MetricDescriptionConstants.RESPONSE_TIME_METRIC);
+		List<Measure<?, Duration>> timeMeasurements = MeasurementTestUtils.allMeasurementsOfMetric(measurement.get(),
+				MetricDescriptionConstants.POINT_IN_TIME_METRIC);
+		assertEquals(responseTimeMeasurements.size(), timeMeasurements.size());
 
-        for (int i = 0; i < 100; i++) {
-            if (i < 20) {
-                assertEquals(expectedMeasurementTime, timeMeasurements.get(i)
-                    .doubleValue(SI.SECOND), 0.001);
-                expectedMeasurementTime += 100.0;
-            }
-            else {
-                assertEquals(expectedMeasurementTime, timeMeasurements.get(i)
-                    .doubleValue(SI.SECOND), 0.001);
-                expectedMeasurementTime += 1.0;
-                System.out.println("expectedMeaurementTime: " + expectedMeasurementTime);
-                System.out.println("Measurement: " + timeMeasurements.get(i)
-                    .doubleValue(SI.SECOND));
-                assertEquals(expectedMeasurementTime, timeMeasurements.get(i + 1)
-                    .doubleValue(SI.SECOND), 0.001);
-                expectedMeasurementTime += 99.0;
-                i++;
-            }
-        }
-        // Check whether all the response time stay at 1.0
-        MeasurementTestUtils.allDoubleMeasurementValuesMatch(measurement.get(),
-                MetricDescriptionConstants.RESPONSE_TIME_METRIC, SI.SECOND, closeTo(1.0, 0.001));
+		double expectedMeasurementTime = 1.0;
+
+		for (int i = 0; i < 100; i++) {
+			if (i < 20) {
+				assertEquals(expectedMeasurementTime, timeMeasurements.get(i).doubleValue(SI.SECOND), 0.001);
+				expectedMeasurementTime += 100.0;
+			} else {
+				assertEquals(expectedMeasurementTime, timeMeasurements.get(i).doubleValue(SI.SECOND), 0.001);
+				expectedMeasurementTime += 1.0;
+				System.out.println("expectedMeaurementTime: " + expectedMeasurementTime);
+				System.out.println("Measurement: " + timeMeasurements.get(i).doubleValue(SI.SECOND));
+				assertEquals(expectedMeasurementTime, timeMeasurements.get(i + 1).doubleValue(SI.SECOND), 0.001);
+				expectedMeasurementTime += 99.0;
+				i++;
+			}
+		}
+		// Check whether all the response time stay at 1.0
+		MeasurementTestUtils.allDoubleMeasurementValuesMatch(measurement.get(),
+				MetricDescriptionConstants.RESPONSE_TIME_METRIC, SI.SECOND, closeTo(1.0, 0.001));
 	}
-	
+
 }
