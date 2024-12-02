7392cc3fb2fa9c366ced5e52ebb67ca9e7c0780d
rsfzi <147811838+rsfzi@users.noreply.github.com>
2024-12-02 20:25:10 +0100

    Revert "adapted to changed base class"
    This reverts commit 6986c358fba814f53327981053ddad1a95fbe25f.

--- bundles/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/launcher/PCMInterpreterLauncher.java
+++ bundles/org.palladiosimulator.simulizar/src/org/palladiosimulator/simulizar/launcher/PCMInterpreterLauncher.java
@@ -1,6 +1,6 @@
 package org.palladiosimulator.simulizar.launcher;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.apache.log4j.Level;
@@ -28,30 +28,30 @@ import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 public class PCMInterpreterLauncher extends SimuComWorkflowLauncher {
 
     private Optional<MDSDBlackboard> blackboard = Optional.empty();
-
+    
     @Override
     protected MDSDBlackboard createBlackboard() {
         return blackboard.orElseGet(super::createBlackboard);
     }
-
+    
     @Override
     protected IJob createWorkflowJob(final SimuComWorkflowConfiguration config, final ILaunch launch)
             throws CoreException {
         if (!(config instanceof SimuLizarWorkflowConfiguration)) {
             throw new IllegalArgumentException("SimuLizarWorkflowConfiguration expected for PCMInterpreterLauncher");
         }
-
+        
         var rootComponent = SimuLizarPlatform.getPlatformComponent()
-            .analysisFactory()
-            .create((SimuLizarWorkflowConfiguration) config);
-
+                .analysisFactory()
+                .create((SimuLizarWorkflowConfiguration) config);
+        
         blackboard = Optional.of(rootComponent.blackboard());
         return rootComponent.rootJob();
     }
 
     @Override
-    protected List<LoggerAppenderStruct> setupLogging(final org.apache.log4j.Level logLevel) throws CoreException {
-        final List<LoggerAppenderStruct> result = super.setupLogging(logLevel);
+    protected ArrayList<LoggerAppenderStruct> setupLogging(final org.apache.log4j.Level logLevel) throws CoreException {
+        final ArrayList<LoggerAppenderStruct> result = super.setupLogging(logLevel);
         result.add(this.setupLogger("org.palladiosimulator.simulizar", logLevel,
                 Level.DEBUG == logLevel ? DETAILED_LOG_PATTERN : SHORT_LOG_PATTERN));
         result.add(this.setupLogger("de.uka.ipd.sdq.probfunction.math.apache.impl", logLevel,
